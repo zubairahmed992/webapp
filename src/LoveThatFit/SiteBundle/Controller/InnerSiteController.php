20f839c668014cd3997f0f8e8a19b5226aa66bec
winCS <swm@winCS>
2013-01-02 22:47:12 +0500

    Inner pages added
    routes added:
    /home & /inner_site/index

---
+++ src/LoveThatFit/SiteBundle/Controller/InnerSiteController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LoveThatFit\SiteBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class InnerSiteController extends Controller {
+
+    //-------------------------------------------------------------------------
+
+    public function indexAction() {
+        return $this->render('LoveThatFitSiteBundle:InnerSite:index.html.twig');
+    }
+
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+?>
