6964dfe06eaa757d4e25cf120926e8ec99fe74db
waqas muddasir <swm@winCS>
2013-02-05 19:54:31 +0500

    about us & contact us pages related stuff

--- src/LoveThatFit/SiteBundle/Controller/HomeController.php
+++ src/LoveThatFit/SiteBundle/Controller/HomeController.php
@@ -13,8 +13,13 @@ class HomeController extends Controller {
     public function indexAction() {
         return $this->render('LoveThatFitSiteBundle:Home:index.html.twig');
     }
+public function aboutUsAction() {
+        return $this->render('LoveThatFitSiteBundle:Home:after_login_about_us.html.twig');
+    }
 
-    
+public function contactUsAction() {
+        return $this->render('LoveThatFitSiteBundle:Home:after_login_contact_us.html.twig');
+    }    
     
     
     
