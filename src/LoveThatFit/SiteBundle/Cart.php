e1ce7c4576d6db1132ba34eb702a652fa3a73e6e
waqas muddasir <swm@winCS>
2013-03-14 17:04:35 +0500

    bits changed in cart class

--- src/LoveThatFit/SiteBundle/Cart.php
+++ src/LoveThatFit/SiteBundle/Cart.php
@@ -11,7 +11,7 @@ class Cart {
         if ($cart)
             $this->cart = $cart;
     }
-
+//---------------------------------------------------------------------
     function getTotal() {
         $total = 0;
         foreach ($this->cart as $key => $value) {
@@ -20,17 +20,26 @@ class Cart {
 
         return $total;
     }
-
+//---------------------------------------------------------------------
     function getCart() {
         return $this->cart;
     }
-
+//---------------------------------------------------------------------
     function removeFromCart($product) {
+       return $this->removeFromCart($product, 1);
+    }
+    
+//---------------------------------------------------------------------
+    function addToCart($product) {
+       return $this->addMultipleToCart($product, 1);
+    }
+//---------------------------------------------------------------------
+    function removeMultipleFromCart($product, $qty) {
 
         foreach ($this->cart as $key => $value) {
             if ($value['id'] == $product->getId()) {
-                if ($value['quantity'] > 1) {
-                    $this->cart[$key]['quantity'] = $this->cart[$key]['quantity'] - 1;
+                if ($value['quantity'] > $qty) {
+                    $this->cart[$key]['quantity'] = $this->cart[$key]['quantity'] - $qty;
                 } else {
                     unset($this->cart[$key]);
                 }
@@ -39,30 +48,30 @@ class Cart {
         }
         return false;
     }
-
-    function addToCart($product) {
+//---------------------------------------------------------------------
+    function addMultipleToCart($product, $qty) {
 
         if ($product) {
-            if (!$this->addToExistingProduct($product)) {
-                $this->addNewProduct($product);
+            if (!$this->addToExistingProduct($product, $qty)) {
+                $this->addNewProducts($product, $qty);
             }
             return true;
         }
         else
             return false;
     }
-
-    private function addToExistingProduct($product) {
+//---------------------------------------------------------------------    
+    private function addToExistingProduct($product, $qty) {
         foreach ($this->cart as $key => $value) {
             if ($value['id'] == $product->getId()) {
-                $this->cart[$key]['quantity'] = $this->cart[$key]['quantity'] + 1;
+                $this->cart[$key]['quantity'] = $this->cart[$key]['quantity'] + $qty;
                 return true;
             }
         }
         return false;
     }
-
-    private function addNewProduct($product) {
+//---------------------------------------------------------------------
+    private function addNewProducts($product, $qty) {
        $imgPath=$product->getImagePaths()['icon'];
         array_push(
                 $this->cart, array(
@@ -70,7 +79,7 @@ class Cart {
             "title" => $product->getName(),
             "brand" => $product->getBrand()->getName(),
             "clothingType" => $product->getClothingType()->getName(),
-            "quantity" => 1,
+            "quantity" => $qty,
             "price" => 99.99,
             "image" => $imgPath,
                 )
