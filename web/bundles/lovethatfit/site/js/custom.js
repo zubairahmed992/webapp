8da59d8af2983d1f15041ecfc0df6d0376aebb73
behlool <behlool@centricsource.com>
2013-04-10 21:06:47 +0500

    Worked on product detail on left slider panel

--- web/bundles/lovethatfit/site/js/custom.js
+++ web/bundles/lovethatfit/site/js/custom.js
@@ -88,3 +88,24 @@ $(document).ready(function(){
 		
 		);
 });
+
+
+
+
+(function($) {
+$(function() {
+
+	$('ul.tabs').each(function() {
+		$(this).find('li').each(function(i) {
+			$(this).click(function(){
+				$(this).addClass('current').siblings().removeClass('current')
+					.parents('div.sectionbox').find('div.box').eq($(this).index()).fadeIn(150).siblings('div.box').hide();
+			});
+		});
+	});
+
+})
+})(jQuery)
+
+
+
