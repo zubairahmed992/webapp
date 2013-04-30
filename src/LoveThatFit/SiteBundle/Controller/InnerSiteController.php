b0ae2f3f698260907dd9411c911e028f21120409
Azeem <azeem.akram@centricsource.com>
2013-04-30 21:57:36 +0500

    Bits Change in InnerSiteController

--- src/LoveThatFit/SiteBundle/Controller/InnerSiteController.php
+++ src/LoveThatFit/SiteBundle/Controller/InnerSiteController.php
@@ -101,7 +101,7 @@ class InnerSiteController extends Controller {
         $user_id=$this->get('security.context')->getToken()->getUser()->getId(); 
         $em = $this->getDoctrine()->getManager();
         $entity = $em->getRepository('LoveThatFitAdminBundle:Product')->findProductItemByUser($user_id , $page_number , $limit);
-        return $this->renderProductTemplateductTemplate($entity , $page_number , $limit);       
+        return $this->renderProductTemplate($entity , $page_number , $limit);       
     }
     //-------------------------------------------------------------------
     public function ajaxAction() {
