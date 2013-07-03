bbac6df85fe295348ac06113126e0bc71879833f
waqas muddasir <swm@winCS>
2013-07-03 23:31:32 +0500

    ajax calls authentication issue related

---
+++ src/LoveThatFit/SiteBundle/Component/Event/AjaxAuthenticationListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LoveThatFit\SiteBundle\Component\Event;
+ 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+/**
+ */
+class AjaxAuthenticationListener
+{
+ 
+    /**
+     * Handles security related exceptions.
+     *
+     * @param GetResponseForExceptionEvent $event An GetResponseForExceptionEvent instance
+     */
+    public function onCoreException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        $request = $event->getRequest();
+ 
+        if ($request->isXmlHttpRequest()) {
+            if ($exception instanceof AuthenticationException || $exception instanceof AccessDeniedException) {
+                $event->setResponse(new Response('beep', 403));
+            }
+        }
+    }
+}
