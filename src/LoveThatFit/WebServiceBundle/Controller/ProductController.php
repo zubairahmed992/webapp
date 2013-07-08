02991618e5e5c09176de906ae9a34e15887dec5e
waqas muddasir <swm@winCS>
2013-07-08 23:09:10 +0500

    changes related to product detail json

--- src/LoveThatFit/WebServiceBundle/Controller/ProductController.php
+++ src/LoveThatFit/WebServiceBundle/Controller/ProductController.php
@@ -23,31 +23,30 @@ class ProductController extends Controller {
 
     public function brandListSizeChartAction() {
         $total_record = count($this->getBrandArray());
-        $data=array();
-        $data['data']=$this->getBrandArray();
+        $data = array();
+        $data['data'] = $this->getBrandArray();
         return new Response($this->json_view($total_record, $data));
     }
+
 #-----------------Size Chart Against The Brand Id For Registration Step2---------------------------------------------------#
-    public function sizeChartsAction(){
-        
-         $handle = fopen('php://input','r');
-         $jsonInput = fgets($handle);
-         $request_array  = json_decode($jsonInput,true);
-         $size_chart_helper=$this->get('admin.helper.sizechart');
-         $size_chart=$size_chart_helper->sizeChartList($request_array);
-         if($size_chart) {
-             $size_chart_data=array();
-             $size_chart_data=$size_chart;
-         $total_record = count($size_chart);
-         return new Response($this->json_view($total_record,$size_chart_data));
-        }  
-        else {
-        return new response(json_encode(array('Message' => 'Can not find Size Chart')));
+
+    public function sizeChartsAction() {
+
+        $handle = fopen('php://input', 'r');
+        $jsonInput = fgets($handle);
+        $request_array = json_decode($jsonInput, true);
+        $size_chart_helper = $this->get('admin.helper.sizechart');
+        $size_chart = $size_chart_helper->sizeChartList($request_array);
+        if ($size_chart) {
+            $size_chart_data = array();
+            $size_chart_data = $size_chart;
+            $total_record = count($size_chart);
+            return new Response($this->json_view($total_record, $size_chart_data));
+        } else {
+            return new response(json_encode(array('Message' => 'Can not find Size Chart')));
         }
-        
-        
-        
-    }            
+    }
+
 #--------------------Brand List-------------------------------------------------------------------------------#
 
     public function brandListAction() {
@@ -63,43 +62,42 @@ class ProductController extends Controller {
         $data['path'] = $baseurl;
         return new Response($this->json_view($total_record, $data));
     }
+
 #----------------------Service for Product --------------------------------------#
-    
-        //------Proudct List By Clothing Type and By Brand  With Gender----------------------///   
-    public function byBrandClothingTypeAction()
-    {
+    //------Proudct List By Clothing Type and By Brand  With Gender----------------------///   
+    public function byBrandClothingTypeAction() {
         $request = $this->getRequest();
         $brand = $this->getDoctrine()
                 ->getRepository('LoveThatFitAdminBundle:Brand')
                 ->findAllBrandWebService();
         $clothing_types = $this->getDoctrine()
                 ->getRepository('LoveThatFitAdminBundle:ClothingType')
-                ->findAllBrandWebService(); 
+                ->findAllBrandWebService();
 
         $total_record = count($brand);
-        
+
         $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . '/uploads/ltf/brands/';
         $data = array();
-       
-        $data['data']=array_merge($brand,$clothing_types);
-       
+
+        $data['data'] = array_merge($brand, $clothing_types);
+
         $data['brand_image_path'] = $baseurl;
-       return new Response($this->json_view($total_record, $data));  
-        
+        return new Response($this->json_view($total_record, $data));
     }
- #-----------------------------Productlist Against Brand or Clothing Type -----------------------------------#   
- public function productlistAction()
- {
-     $request = $this->getRequest();
-     $handle = fopen('php://input','r');
-     $jsonInput = fgets($handle);
-     $request_array  = json_decode($jsonInput,true);
-     $id=$request_array['id'];
-     $type=$request_array['type'];
-     $gender=$request_array['gender'];
-   
-     $products=Null;
-     if ($type == "brand") {
+
+    #-----------------------------Productlist Against Brand or Clothing Type -----------------------------------#   
+
+    public function productlistAction() {
+        $request = $this->getRequest();
+        $handle = fopen('php://input', 'r');
+        $jsonInput = fgets($handle);
+        $request_array = json_decode($jsonInput, true);
+        $id = $request_array['id'];
+        $type = $request_array['type'];
+        $gender = $request_array['gender'];
+
+        $products = Null;
+        if ($type == "brand") {
             $products = $this->getDoctrine()
                     ->getRepository('LoveThatFitAdminBundle:Product')
                     ->findProductByBrandWebService($id, $gender);
@@ -118,115 +116,103 @@ class ProductController extends Controller {
             $products = $this->getDoctrine()
                     ->getRepository('LoveThatFitAdminBundle:Product')
                     ->findLattestProductWebService($gender);
-        }    
-       $data=array();
-   #-------Fetching The Path------------#
-     if($products) {
-       /* if($products[0]['product_image'] )
-       { 
-          $product_id=$products[0]['id'];
-          $productimage_path = $this->getDoctrine()
-                ->getRepository('LoveThatFitAdminBundle:Product')
-                 ->find($product_id);
-        $images_path= $productimage_path->getdefalutImagePaths(); 
-        $data['path']=$images_path;
-       }*/
-   
-      
-      $total_record=count($products);
-      $data['data']=$products;
-      $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath().'/uploads/ltf/products/';
-      $data['path']=$baseurl;
-      return new Response($this->json_view($total_record, $data)); 
+        }
+        $data = array();
+        #-------Fetching The Path------------#
+        if ($products) {
+            /* if($products[0]['product_image'] )
+              {
+              $product_id=$products[0]['id'];
+              $productimage_path = $this->getDoctrine()
+              ->getRepository('LoveThatFitAdminBundle:Product')
+              ->find($product_id);
+              $images_path= $productimage_path->getdefalutImagePaths();
+              $data['path']=$images_path;
+              } */
+
+
+            $total_record = count($products);
+            $data['data'] = $products;
+            $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . '/uploads/ltf/products/';
+            $data['path'] = $baseurl;
+            return new Response($this->json_view($total_record, $data));
+        } else {
+            return new Response(json_encode(array('Message' => 'We can not find Product')));
+        }
     }
-    else
-    {
-          return new Response(json_encode(array('Message'=>'We can not find Product'))); 
-    }
-     
- }   
+
 #--------------------Product Detail -------------------------------------------------------------#
- //------Proudct List By Product Detail----------------------///   
-    public function productDetailAction()
-    {
-       $request = $this->getRequest();
-       $request = $this->getRequest();
-     $handle = fopen('php://input','r');
-     $jsonInput = fgets($handle);
-     $request_array  = json_decode($jsonInput,true);
-     $product_id=$request_array['id'];
-       $em = $this->getDoctrine()->getManager();
-       $product_id=2;
-       $productdetail=array();
-       $products = $this->getDoctrine()
+    //------Proudct List By Product Detail----------------------///   
+
+    public function productDetailAction() {
+        
+        $request = $this->getRequest();
+        $handle = fopen('php://input', 'r');
+        $jsonInput = fgets($handle);
+        $request_array = json_decode($jsonInput, true);
+        
+        $product_id = $request_array['id'];
+        $em = $this->getDoctrine()->getManager();
+        
+        $product_id = 2;
+        
+        $productdetail = array();
+        $products = $this->getDoctrine()
                 ->getRepository('LoveThatFitAdminBundle:Product')
                 ->productDetail($product_id);
-     
+
         $product = $this->getDoctrine()
                 ->getRepository('LoveThatFitAdminBundle:Product')
                 ->find($product_id);
-        $count_rec=count($products); 
-        $productdetail['product']=$products;
-         $product_color_array=array();
-     
+        
+        $count_rec = count($products);
+        $productdetail['product'] = $products;
+        
+        $product_color_array = array();
+
         #-- FOR COLORS AND SIZE----------
-        if($count_rec>0)
-        { 
-          
-         $product_colors = $product->getProductColors();
-         $product_size_id=null;
-         $size_id = null;
-          foreach($product_colors as $product_color_value)
-          {
-               $product_color_id = $product_color_value->getId();
-                
-               $color_sizes = $this->getDoctrine()
+        if ($count_rec > 0) {
+
+            $product_colors = $product->getProductColors();
+            $product_size_id = null;
+            $size_id = null;
+            foreach ($product_colors as $product_color_value) {
+                $product_color_id = $product_color_value->getId();
+
+                $color_sizes = $this->getDoctrine()
                         ->getRepository('LoveThatFitAdminBundle:ProductColor')
                         ->getSizeItemImageUrlArray($product_color_id);
-                
-                $color_size_array=array();
-             
-                foreach($color_sizes as $cs)
-          {
-                    array_push($color_size_array, Array($cs['title'] => $cs));
-                  
-          }
-                
-           $product_color_array[$product_color_value->gettitle()]= array(
-                'id'=>$product_color_value->getId(),
-                'image'=>$product_color_value->getImage(),
-                'pattern'=>$product_color_value->getPattern(),
-               'title'=>$product_color_value->getTitle(),
-               'sizes'=>$color_size_array,
-           );
-           
-          
-                
-                  
-            }  
-            $productdetail['product_color']=$product_color_array;
-            $data= array();
-        $data['data']=$productdetail;
-        $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath().'/uploads/ltf/products/';
-        $fitting_room = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath().'/uploads/ltf/products/fitting_room/';
-        $pattern = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath().'/uploads/ltf/products/pattern/';
-    
-        $data['product_color_path']=$baseurl;
-        $data['fitting_room_path']=$fitting_room;
-        $data['pattern_path']=$pattern;
-        
-        return new Response($this->json_view($count_rec,$data));
-        
+
+                $color_size_array = array();
+
+                foreach ($color_sizes as $cs) {
+                    $color_size_array [$cs['title']] = $cs;
+                }
+
+                $product_color_array[$product_color_value->gettitle()] = array(
+                    'id' => $product_color_value->getId(),
+                    'image' => $product_color_value->getImage(),
+                    'pattern' => $product_color_value->getPattern(),
+                    'title' => $product_color_value->getTitle(),
+                    'sizes' => $color_size_array,
+                );
+            }
+            $productdetail['product_color'] = $product_color_array;
+            $data = array();
+            $data['data'] = $productdetail;
+            $baseurl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . '/uploads/ltf/products/';
+            $fitting_room = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . '/uploads/ltf/products/fitting_room/';
+            $pattern = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath() . '/uploads/ltf/products/pattern/';
+
+            $data['product_color_path'] = $baseurl;
+            $data['fitting_room_path'] = $fitting_room;
+            $data['pattern_path'] = $pattern;
+
+            return new Response($this->json_view($count_rec, $data));
+        } else {
+            return json_encode(array('Message' => 'Record Not Found'));
         }
-        else
-        {
-           return json_encode(array('Message' => 'Record Not Found'));  
-            
-        }
-        
-       
     }
-    
 
 #---------------------------Render Json--------------------------------------------------------------------#
 
@@ -249,9 +235,9 @@ class ProductController extends Controller {
                 ->getBrandList();
 
         $brands_array = array();
-       
+
         foreach ($brands as $i) {
-            array_push( $brands_array,array('id'=>$i['id'],'brand_name'=>$i['name']));
+            array_push($brands_array, array('id' => $i['id'], 'brand_name' => $i['name']));
         }
         return $brands_array;
     }
