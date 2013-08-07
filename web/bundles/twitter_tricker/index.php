10e5fc8c7c04b7cee3129d57f30f2723be75660d
suresh <suresh.kumar@centricsource.com>
2013-08-07 04:15:19 -0700

    Twitter Ticker script

---
+++ web/bundles/twitter_tricker/index.php
@@ -0,0 +1,86 @@
+<!-- 
+	Name: Twitter Tricker
+	Description: Twitter style vertical Ticker.
+	Version : 1.00
+	Author: Suresh Kumar 
+	Email: skamrani2002@gmail.com
+	Demo: http://lovethatfit.com
+	Copyright: Copyright (c) 2013
+-->
+<!-- jQuery UI widget factory --> 
+<script src="jquery.js" type="text/javascript"></script>
+<script src="jquery.vticker-min.js" type="text/javascript"></script>  
+<style>
+h1{padding:30px;color:#FFF;}
+#tweet_cont{padding:50px ; }
+#tweet_cont ul {margin:0;padding:0;list-style:none; }
+#tweet_cont ul .tweet {background: #fff; margin-bottom:3px;clear:both;text-align:left; padding:3px;width:590px; }
+#tweet_cont ul .tweet .avatar{float:left;width:48px;height:48px;margin:0px 5px 0px 0px; border:none;} 
+#tweet_cont ul .tweet  h4{margin:0px 0px 0px 0px; font-size:14px; margin:0;padding:0;font-family:arial; }
+#tweet_cont ul .tweet   small{padding-left:50px; font-size:13px; margin:0;padding:0;font-family:arial; font-weight:normal;}
+.clearboth {clear: both;}
+a {color:#3E76C2;text-decoration:none;}
+a:hover {text-decoration:underline;}
+</style>
+
+
+<script type="text/javascript"> 
+$(function(){
+	$('.tweets').vTicker({ 
+		speed: 500,
+		pause: 3000,
+		animation: 'fade',
+		mousePause: true,
+		showItems: 1,
+		direction: 'down',
+		height:74
+	});
+});
+</script>
+
+<div class="tweets" >
+
+<?php 
+
+session_start();
+require_once("twitteroauth.php"); //Path to twitteroauth library
+ 
+$twitteruser = 'LoveThatFit';
+$notweets = 10;
+$consumerkey = "ei0sUDMZxUCLLGOYVB6GUg";
+$consumersecret = "3MugBhG2PDafRMOshiS58ShVJ5gYrA2EWVdTRX0vvvI";
+$accesstoken = "67584638-ee2hjCDOgudYUoGudYOGfuBReTY8lkSyzlweSmtKK";
+$accesstokensecret = "gac1LgG0YoDjAWDd3AV9BmbYB0ofxlGEwCDZpj3UyU";
+ 
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+  
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+ 
+$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+
+
+//echo json_encode($tweets);
+?>
+<ul>
+
+<?php
+$count=1; 
+foreach($tweets as $ind_tweets){?>
+
+<li id="hide_<?php echo $count;?>">
+<div class="tweet">
+
+<img src="<?php echo $ind_tweets->user->profile_image_url;?> ">
+<?php echo $ind_tweets->text;?> 
+<div class="clearboth"></div>
+</div>
+</li>
+<?php 
+$count++;
+
+}?>
+</ul>
+          
