8f2a46492d6a78f6c8f8c65d67acf49c10e1f403
suresh <suresh.kumar@centricsource.com>
2013-09-06 06:18:52 -0700

    Device Type added at product list web service

--- src/LoveThatFit/UserBundle/Entity/UserHelper.php
+++ src/LoveThatFit/UserBundle/Entity/UserHelper.php
@@ -186,6 +186,10 @@ class UserHelper {
     private function countByGender($gender) {
         return  count($this->repo->findUserByGender($gender));        
     }
+   #---------------------------------------------------------------------------#
+    public function findByAuthToken($auth_token){
+        return $this->repo->loadUserByAuthToken($auth_token);
+    }
     
     #---------------------------------------------------------------
 // only use in website for security context in login
