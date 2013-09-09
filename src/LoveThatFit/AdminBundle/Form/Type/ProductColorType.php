b156b402a81fb480986b0d5a1299e89dee183ef6
suresh <suresh.kumar@centricsource.com>
2013-09-09 06:22:54 -0700

    Modified the product color form

--- src/LoveThatFit/AdminBundle/Form/Type/ProductColorType.php
+++ src/LoveThatFit/AdminBundle/Form/Type/ProductColorType.php
@@ -11,7 +11,7 @@ class ProductColorType extends AbstractType
     
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $sizes = array('00'=>'00/XXS', '0'=>'0/XS', '2'=>'2/XS', '4'=>'4/S', '6'=>'6/S', '8'=>'8/M', '10'=>'10/M', '12'=>'12/L','14'=>'14/L','16'=>'16/XL','18'=>'18/XL','20'=>'20/XXL');
+        $sizes = array('00'=>'00/XXS', '0'=>'0/XS', '2'=>'02/XS', '4'=>'04/S', '6'=>'06/S', '8'=>'08/M', '10'=>'10/M', '12'=>'12/L','14'=>'14/L','16'=>'16/XL','18'=>'18/XL','20'=>'20/XXL');
         $displayProductColor=array('Yes'=>'Yes');
         $builder->add('title');
         $builder->add('tempImage','hidden');
