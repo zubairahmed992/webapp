8a6d135663f02ad725064dc83927117334da8c03
suresh <suresh.kumar@centricsource.com>
2013-09-16 16:45:40 +0500

    Bits at product controller

--- src/LoveThatFit/AdminBundle/Controller/ProductController.php
+++ src/LoveThatFit/AdminBundle/Controller/ProductController.php
@@ -33,7 +33,7 @@ class ProductController extends Controller {
 //---------------------------------------------------------------------
     
     public function indexAction($page_number, $sort = 'id') {
-        //$this->productSaveYaml();
+        $this->productSaveYaml();
         $product_with_pagination = $this->get('admin.helper.product')->getListWithPagination($page_number, $sort);
         return $this->render('LoveThatFitAdminBundle:Product:index.html.twig', $product_with_pagination);
     }
@@ -799,7 +799,7 @@ public function productStatsAction()
             foreach ($product->getProductSizes() as $productSizes) {
         $products['products'][$brand_array->getName()][$clothing_type_array->getName()][$product->getName()]['product_sizes'][$productSizes->getTitle()] = array('title' => $productSizes->getTitle(), 'inseam_min' => $productSizes->getInseamMin(), 'inseam_max' => $productSizes->getInseamMax(), 'hip_min' => $productSizes->getHipMin(), 'hip_max' => $productSizes->getHipMax(), 'waist_min' => $productSizes->getWaistMin(), 'waist_max' => $productSizes->getWaistMax(), 'bust_min' => $productSizes->getBustMin(), 'bust_max' => $productSizes->getBustMax(),);  
             foreach ($product->getProductItems() as $productItem) {
-             $products['products'][$brand_array->getName()][$clothing_type_array->getName()][$product->getName()]['product_item'][$productColor->getTitle()][$productItem->getLineNumber()] = array('size_title' => $productItem->getLineNumber(), 'product_color_title' => $productColor->getTitle(), 'image' => $productItem->getImage());
+             $products['products'][$brand_array->getName()][$clothing_type_array->getName()][$product->getName()]['product_item'][$productColor->getTitle()][$productSizes->getTitle()] = array('size_title' => $productSizes->getTitle(), 'product_color_title' => $productColor->getTitle(), 'image' => $productItem->getImage());
                             }
             } 
                 
