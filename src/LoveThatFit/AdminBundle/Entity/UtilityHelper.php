2da287bfc95f22ef73a8a10c285e248ea2413800
waqas muddasir <swm@winCS>
2013-09-21 22:33:53 +0500

    size titles changed & sorted for product detail

--- src/LoveThatFit/AdminBundle/Entity/UtilityHelper.php
+++ src/LoveThatFit/AdminBundle/Entity/UtilityHelper.php
@@ -77,5 +77,65 @@ public function getSizeNumbers()
     
 }
 //--------------------------------------------------------------------------------
+public function setSizeTitles($sizes)
+{ $new_sizes=array();
+    $new_key='';
+    foreach ($sizes as $key => $value) {
+        switch ($value){
+            case "0":
+                $new_key="XXS : ".$value;
+                break;
+            case "1":
+                $new_key="XS : ".$value;
+                break;
+            case "2":
+                $new_key="XS : ".$value;
+                break;
+            case "4":
+                $new_key="S : ".$value;
+                break;
+            case "6":
+                $new_key="S : ".$value;
+                break;
+            case "8":
+                $new_key="M : ".$value;
+                break;
+            case "10":
+                $new_key="M : ".$value;
+                break;
+            case "12":
+                $new_key="L : ".$value;
+                break;
+            case "14":
+                $new_key="L : ".$value;
+                break;
+            case "16":
+                $new_key="XL : ".$value;
+                break;
+            case "18":
+                $new_key="XL : ".$value;
+                break;
+            case "20":
+                $new_key="XXL : ".$value;
+                break;
+            case "22":
+                $new_key="XXL : ".$value;
+                break;
+            case "24":
+                $new_key="XXL : ".$value;
+                break;
+            case "26":
+                $new_key="XXL : ".$value;
+                break;
+            case "28":
+                $new_key="XXL : ".$value;
+                break;
+        }
+    
+        $new_sizes[$key]=$new_key;
+    }
+    return $new_sizes;
+}
+
 
 }
