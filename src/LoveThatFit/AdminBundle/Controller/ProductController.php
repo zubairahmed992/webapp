5f870e5427a4c9a168bdff1d8c3088b6a3ae768a
waqas muddasir <swm@winCS>
2013-12-24 23:22:48 +0500

    product data CSV parsing

--- src/LoveThatFit/AdminBundle/Controller/ProductController.php
+++ src/LoveThatFit/AdminBundle/Controller/ProductController.php
@@ -888,18 +888,133 @@ public function productSizeMeasurementCreateAction($id,$size_id,$title)
       $filename=$file->getData();
       $row = 1;
       if (($handle = fopen($filename, "r")) !== FALSE) {
+          
       while (($data = fgetcsv($handle, 100000, ",")) !== FALSE) {
       $num = count($data);
       echo "<p> $num fields in line $row: <br /></p>\n";
+      if ($row==1){
+          echo $data[2];
+      }
+      
       $row++;
         for ($c=0; $c < $num; $c++) {
-        echo $data[$c] . "<br />\n";
+        echo $data[$c] . "\n";
          }
       }
       fclose($handle);
       }
    }
+    //------------------------------------------------------
+   
+    public function _readProductCsvAction(){
+        $row = 0;
+        $previous_row='';
+    if (($handle = fopen("../app/config/LaceBlouse.csv", "r")) !== FALSE) {
     
-    
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);        
+       
+         if ($row==0){
+            echo $data[0].' ' .$data[1] . "<br />\n"; #~~~~~ Garment Name
+            echo $data[3].' ' .$data[4] . "<br />\n"; #~~~~~ Retailer
+            echo $data[6].' ' .$data[7] . "<br />\n"; #~~~~~ Style
+        }
+        
+        if ($row==11){
+            echo $data[0].' : ' .$data[1] . "<br />\n"; #~~~~~ Stretch
+            echo $data[2].' : ' .$data[3] . "<br />\n"; #~~~~~ H-Stretch
+            echo $data[4].' : ' .$data[5] . "<br />\n"; #~~~~~ V-Stretch
+        }
+        
+        if ($row==13){
+            echo $data[0].' : ' .$data[1] . "<br />\n"; #~~~~~ Fabric Weight
+            echo $data[3].' : ' .$data[4] . "<br />\n"; #~~~~~ Structural Detail
+            echo $data[6].' : ' .$data[7] . "<br />\n"; #~~~~~ Styling Detail
+        }
+        if ($row==15){
+            echo $data[0].' : ' .$data[1] . "<br />\n"; #~~~~~ Fit Type
+            echo $data[3].' : ' .$data[4] . "<br />\n"; #~~~~~ Layering            
+        }        
+        #~~~~~ Fit Priority
+        if ($row==18){    
+            echo "<b>Fit Priority :</b>" ;
+            echo $data[1]. $previous_row[2]. ' : ' . $data[2]. $previous_row[3]. ' : ' . $data[3]. $previous_row[4]. ' : ' . $data[4]. $previous_row[5]. ' : ' . $data[5]. $previous_row[6]. ' : ' . $data[6]. $previous_row[7]."<br />\n";            
+        } 
+        #~~~~~ Colors
+        if ($row==25){    
+            echo "<b>Colors :</b>" ;
+            echo $data[1] .', '. $data[2].', '. $data[3] .', '. $data[4].', '. $data[5].', '. $data[6] .', '. $data[7].', '. $data[8].', '. $data[9].', '. $data[10].', '. $data[11]."<br />\n";            
+        } 
+        #---------- Fabric Content
+        if ($row==28){    
+            echo "<b>Fabric Content </b><br/>";            
+            echo $data[1] .' : '. $data[0] . "<br />\n";
+            echo $data[3] .' : '. $data[2] . "<br />\n";
+            echo $data[5] .' : '. $data[4] . "<br />\n";
+            echo $data[7] .' : '. $data[6] . "<br />\n";
+            echo $data[9] .' : '. $data[8] . "<br />\n";
+            echo $previous_row[1] .' : '. $previous_row[0] . "<br />\n";
+            echo $previous_row[3] .' : '. $previous_row[2] . "<br />\n";
+            echo $previous_row[5] .' : '. $previous_row[4] . "<br />\n";
+            echo $previous_row[7] .' : '. $previous_row[6] . "<br />\n";
+            echo $previous_row[9] .' : '. $previous_row[8] . "<br />\n";            
+          } 
+        $this->getCSVSizeDetail($data, $row);
+        $previous_row=$data;
+        
+        $row++;
+        /*
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }*/
+    }
+    fclose($handle);
+    //return new Response(json_encode($data[$c]));
+    return new Response('true');
 }
+      
+    
+    }
+    
+    private function getCSVSizeDetail($data, $row){
+       if ($row>=5 && $row<=22){
+        $this->getCSVFields($data, $row);
+       }
+    }
+    private function getCSVFields($data, $row){
+       
+        echo $data[24]." ".$data[32]." ".$data[47]." ".$data[54]." ".$data[63]." ".$data[71]." ".$data[79]." ".$data[87]." ".$data[95]."<br>";
+       
+    }
 
+    //------------------------------------------------------
+   
+    public function readProductCsvAction() {
+        $row = 0;
+        $previous_row = '';
+        if (($handle = fopen("../app/config/LaceBlouse.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {                
+               if ($row >= 5 && $row <= 22) {
+                        #garment_measurement_flat	stretch_type_percentage	garment_measurement_stretch_fit	maximum_body_measurement ideal_body_size_high | ideal_body_size_low			
+                        echo "00  |" . $data[23].":".$data[25].":".$data[26].":".$data[27].":".$data[28].":".$data[29].":".$data[30]."<br>";
+                        echo "0   |" . $data[31].":".$data[32].":".$data[33].":".$data[34].":".$data[35].":".$data[36].":".$data[37]."<br>";
+                        echo "2   |" . $data[39].":".$data[33].":".$data[34].":".$data[35].":".$data[36].":".$data[37].":".$data[38]."<br>";
+                        echo "4   |" . $data[47].":".$data[48].":".$data[49].":".$data[50].":".$data[51].":".$data[52].":".$data[53]."<br>";
+                        echo "6   |" . $data[55].":".$data[56].":".$data[57].":".$data[58].":".$data[59].":".$data[60].":".$data[61]."<br>";
+                        echo "8   |" . $data[63].":".$data[64].":".$data[65].":".$data[66].":".$data[67].":".$data[68].":".$data[69]."<br>";
+                        echo "10  |" . $data[71].":".$data[72].":".$data[73].":".$data[74].":".$data[75].":".$data[76].":".$data[77]."<br>";
+                        echo "12  |" . $data[79].":".$data[80].":".$data[81].":".$data[82].":".$data[83].":".$data[84].":".$data[85]."<br>";
+                        echo "14  |" . $data[87].":".$data[88].":".$data[89].":".$data[90].":".$data[91].":".$data[92].":".$data[93]."<br>";
+                        echo "16  |" . $data[95].":".$data[96].":".$data[97].":".$data[98].":".$data[99].":".$data[100].":".$data[101]."<br>";
+
+                }
+                echo "<br>";
+                $previous_row = $data;
+                $row++;
+            }
+            fclose($handle);
+            return new Response('true');
+        }
+      
+}
+}
