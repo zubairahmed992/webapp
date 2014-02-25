0d8ac1ed2a8a1b238937bbe9b94548e6ae9e48f9
waqas muddasir <swm@winCS>
2014-02-25 23:21:07 +0500

    algorithm summary

--- src/LoveThatFit/SiteBundle/Comparison.php
+++ src/LoveThatFit/SiteBundle/Comparison.php
@@ -8,20 +8,20 @@ class Comparison {
 
     private $user;
     private $product;
-
+#-----------------------------------------------------
     function __construct($user = null, $product = null) {
         $this->user = $user;
         $this->product = $product;
     }
-
+#-----------------------------------------------------
     function getFeedBackJSON() {
         return json_encode($this->array_mix());
     }
-
+#-----------------------------------------------------
     function getComparison() {
         return $this->array_mix();
     }
-
+#-----------------------------------------------------
     private function array_mix() {
         $sizes = $this->product->getProductSizes();
         $body_specs = $this->user->getMeasurement()->getArray();
@@ -41,39 +41,20 @@ class Comparison {
                         #if true then keep the old value else assign false ~~~>
                         $fb[$size->getTitle()]['fits'] = $it_fits? $fb[$size->getTitle()]['fits']: $it_fits;                
                      
-                        $v = $fb[$size->getTitle()][$fp_specs['fit_point']]['variance'];
-                        $m_v = $fb[$size->getTitle()][$fp_specs['fit_point']]['max_variance'];
-                        
-                        if (($variance>=0 && $v>=0)|| ($variance<=0 && $v<=0)){
-                              $variance=$variance+$v;    
-                        }elseif ($variance<0 && $v>0){
-                             $variance=$variance;    
-                        }else{
-                             $variance=$v;    
-                        }
-                        
-                        if (($max_variance>=0 && $m_v>=0) ||($max_variance<=0 && $m_v<=0)){
-                            $max_variance=$max_variance+$m_v;
-                        }elseif ($max_variance<0 && $m_v>0){
-                            $max_variance=$max_variance;
-                        }else{
-                            $max_variance=$m_v;
-                        }
+                        $max_variance=  $this->get_accumulated_variance($max_variance, $fb[$size->getTitle()][$fp_specs['fit_point']]['max_variance']);
+                        $variance = $this->get_accumulated_variance($variance, $fb[$size->getTitle()][$fp_specs['fit_point']]['variance']);                      
                 }
             }
                 $fb[$size->getTitle()]['message'] = $fb[$size->getTitle()]['fits']?' fits~~~~~* ': ' v:'. $variance.' mv:'. $max_variance;
         }
         return $fb;
     }
-    
+#-----------------------------------------------------    
     private function get_fit_point_array($fp_specs, $body_specs) {
 
         $body_measurement = array_key_exists($fp_specs['fit_point'], $body_specs) ? $body_specs[$fp_specs['fit_point']] : 0;
 
-        $fits=false;
-        if ($body_measurement >= $fp_specs['ideal_body_size_low'] && 
-                    $body_measurement<=$fp_specs['ideal_body_size_high']) $fits=true;
-        $variance=$this->calculate_variance($body_measurement, $fp_specs['ideal_body_size_high'],
+        $variance=$this->get_variance($body_measurement, $fp_specs['ideal_body_size_high'],
                     $fp_specs['ideal_body_size_low'], $fp_specs['max_body_measurement'], $fp_specs['fit_priority']);
         
         return array('fit_point' => $fp_specs['fit_point'],
@@ -82,53 +63,74 @@ class Comparison {
             'max_body_measurement' => $fp_specs['max_body_measurement'],
             'fit_priority' => $fp_specs['fit_priority'],
             'body_measurement' => $body_measurement,
-            'fits' => $fits,
+            'fits' => $variance[3]==1?true:false,
             'variance' => $variance[0],
             'max_variance' => $variance[1],
-            'status'=>$variance[2],
+            'message'=>$variance[2],
+            'status'=>$variance[3],
         );
     }
-    private function calculate_variance($body, $high, $low, $max, $priority){
-        
-        $mid_of_high_max = ($max + $high) / 2;
-        $diff=0;
-        $diff_percent = 0;
-        $m_diff=0;
-        $m_diff_percent = 0;
-        $status='';
-        
-        if($body>=$low && $body<=$high){ #perfect
-            $diff=0;
-            $status='ideal';
-        }elseif($body<$low){ #loose
-            $status='loose';
-            $diff=$low-$body;
-            $diff_percent = ($diff / $low) * 100;
-            $m_diff=$diff;
-            $m_diff_percent = $diff_percent;
-        }elseif($body>$high && $body<$mid_of_high_max){ #high max 1st half    
-            $status='1st half';
-            $diff=$high-$body;
-            $diff_percent = ($diff / $high) * 100;
-            $m_diff=$mid_of_high_max-$body;
-            $m_diff_percent = ($m_diff / $mid_of_high_max) * 100;
-        }elseif($body>$mid_of_high_max && $body<$max){ #high max 2nd half
-            $status='2st half';
-            $diff=$high-$body;
-            $diff_percent = ($diff / $high) * 100;
-            $m_diff=$mid_of_high_max-$body;
-            $m_diff_percent = ($m_diff / $mid_of_high_max) * 100;
-        }elseif($body>=$max){ #not fitting
-            $status='beyond max';
-            $diff=$high-$body;
-            $diff_percent = ($diff / $high) * 100;
-            $m_diff=$mid_of_high_max-$body;
-            $m_diff_percent = ($m_diff / $mid_of_high_max) * 100;
+    
+#-----------------------------------------------------
+    private function get_variance($body, $high, $low, $max, $priority) {
+
+        if ($body==0 || $high==0 || $low==0 || $max==0 || $priority==0 || $body==null || $high==null || $low==null || $max==null || $priority==null){
+            return array(null, null, 'missing values', 0);
         }
-        $varience = number_format(($priority * $diff_percent) / 100, 2, '.', '');
-        $max_varience = number_format(($priority * $m_diff_percent) / 100, 2, '.', '');
-                
-        return array($varience, $max_varience, $status);
+        $mid_of_high_max = ($max + $high) / 2;
+        $varience = 0;
+        $max_varience = 0;
+        $status = 0;
+        $message = '';
+
+        if ($body >= $low && $body <= $high) { #perfect
+            $varience = 0;
+            $max_varience = 0;
+            $status = 1;
+            $message = 'ideal';
+        } elseif ($body < $low) { #loose
+            $message = 'loose';
+            $status = 2;
+            $varience = $this->calculate_variance($body, $low, $priority);
+            $max_varience = $this->calculate_variance($body, $low, $priority);
+        } else {
+            if ($body > $high && $body < $mid_of_high_max) { #high max 1st half    
+                $message = '1st half';
+                $status = -1;
+            } elseif ($body > $mid_of_high_max && $body < $max) { #high max 2nd half
+                $message = '2nd half';
+                $status = -2;
+            } elseif ($body >= $max) { #not fitting
+                $message = 'beyond max';
+                $status = -3;
+            }
+            $varience = $this->calculate_variance($body, $high, $priority);
+            $max_varience = $this->calculate_variance($body, $mid_of_high_max, $priority);
+        }
+
+        return array($varience, $max_varience, $status, $message);
     }
- 
+    
+    #----------------------------------------------------------
+    private function calculate_variance($body, $item, $priority) {
+        if ($item > 0 && $body > 0) {
+            $diff = $item - $body;
+            $diff_percent = ($diff / $item) * 100;
+            return number_format(($priority * $diff_percent) / 100, 2, '.', '');
+        }else
+            return;
+    }
+    
+    #----------------------------------------------------------
+    private function get_accumulated_variance($accumulated, $current) {
+        if (($accumulated >= 0 && $current >= 0) || ($accumulated <= 0 && $current <= 0)) {
+            $accumulated = $accumulated + $current;
+        } elseif ($accumulated < 0 && $current > 0) {
+            $accumulated = $accumulated;
+        } else {
+            $accumulated = $current;
+        }
+        return $accumulated;
+    }
+    
 }
