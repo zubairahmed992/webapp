e539d59527b9449325177e22e0e4f58c0cc3db14
Muhammad Ibrahim Masudi <muhammad.ibrahim@centricsource.com>
2014-04-23 22:20:10 +0500

    To Sync

--- web/bundles/lovethatfit/site/js/default_mannequin.js
+++ web/bundles/lovethatfit/site/js/default_mannequin.js
@@ -27,30 +27,66 @@ function set_default_canv(){
 }
 $(document).ready(function() { 
    //set_default_canv();
-   var actual_user_height = parseInt($("#hdn_user_height").attr("value")) * 6;
-   $("#dm_part_1 img").height((7*actual_user_height)/100);
-   $("#dm_part_2 img").height((46.78*actual_user_height)/100);
-   $("#dm_part_3 img").height((18.83*actual_user_height)/100);
-   $("#dm_part_4 img").height((6.95*actual_user_height)/100);
-   $("#dm_part_5 img").height((3.15*actual_user_height)/100);
-   $("#dm_part_6 img").height(29);
-   $("#dm_part_7 img").height((7.7*actual_user_height)/100);
-   $("#dm_part_8").height((13.74*actual_user_height)/100);   
-   $("#dm_part_8 img").height((13.74*actual_user_height)/100);    
    
-   $("#dm_rgt_arm_hldr").css("top",(21.44*actual_user_height)/100);
-   $("#dm_lft_arm_hldr").css("top",(21.44*actual_user_height)/100);
+   create_default_mannequin(
+    );
    
-   $("#dm_rgt_arm_part_1 img").height($("#dm_part_6 img").height());
-   $("#dm_lft_arm_part_1 img").height($("#dm_part_6 img").height());
+   function create_default_mannequin() {
+        var actual_user_height = parseInt($("#hdn_user_height").attr("value")) * 6;
    
-   $("#dm_rgt_arm_part_2 img").height((35*actual_user_height)/100);
-   $("#dm_lft_arm_part_2 img").height((35*actual_user_height)/100);
+        var mid_width_final = 76;
+        var lft_width_final = 10;
+        var rgt_width_final = 10;
+
+        var full_mid_width_final = mid_width_final + lft_width_final + rgt_width_final;
+        var arm_gap_adj = (lft_width_final + rgt_width_final)/5;
+
+        var rgt_arm_width = mid_width_final / 1.82;
+        var lft_arm_width = mid_width_final / 1.82;
+        var full_body_width = rgt_arm_width + full_mid_width_final + lft_arm_width - (arm_gap_adj * 2);
+
+        $("#dm_mid_body_hldr").width(full_mid_width_final);
+        $("#dm_mid_body_hldr").css({marginLeft: -arm_gap_adj, marginRight: -arm_gap_adj});
+        $("#dm_part_7 .dm_part_img").width(full_mid_width_final);
+        $("#dm_part_2 .dm_part_img").width(full_mid_width_final);
+        $("#dm_part_1 .dm_part_img").width(full_mid_width_final);
+
+        $(".dm_part_mid img").width(mid_width_final);
+        $(".dm_part_lft img").width(lft_width_final);
+        $(".dm_part_rgt img").width(rgt_width_final);
+
+        $("#dm_rgt_arm_hldr").width(rgt_arm_width);
+        $("#dm_lft_arm_hldr").width(lft_arm_width);
+
+        $("#dm_lft_arm_hldr #dm_lft_arm_part_1 .dm_part_img").width(lft_arm_width);
+        $("#dm_lft_arm_hldr #dm_lft_arm_part_2 .dm_part_img").width(lft_arm_width);
+
+        $("#dm_rgt_arm_hldr #dm_rgt_arm_part_1 .dm_part_img").width(rgt_arm_width);
+        $("#dm_rgt_arm_hldr #dm_rgt_arm_part_2 .dm_part_img").width(rgt_arm_width);
+
+        $("#default_dummy_hldr #default_dummy_inner").width(full_body_width);
+
+        $("#dm_part_1 img").height((7*actual_user_height)/100);
+        $("#dm_part_2 img").height((46.78*actual_user_height)/100);
+        $("#dm_part_3 img").height((18.83*actual_user_height)/100);
+        $("#dm_part_4 img").height((6.95*actual_user_height)/100);
+        $("#dm_part_5 img").height((3.15*actual_user_height)/100);
+        $("#dm_part_6 img").height(29);
+        $("#dm_part_7 img").height((7.7*actual_user_height)/100);
+        $("#dm_part_8").height((13.74*actual_user_height)/100);   
+        $("#dm_part_8 img").height((13.74*actual_user_height)/100);    
+
+
+
+        $("#dm_rgt_arm_hldr").css("top",(21.44*actual_user_height)/100);
+        $("#dm_lft_arm_hldr").css("top",(21.44*actual_user_height)/100);
+
+        $("#dm_rgt_arm_part_1 img").height($("#dm_part_6 img").height());
+        $("#dm_lft_arm_part_1 img").height($("#dm_part_6 img").height());
+
+        $("#dm_rgt_arm_part_2 img").height((35*actual_user_height)/100);
+        $("#dm_lft_arm_part_2 img").height((35*actual_user_height)/100);
+   }
    
-   //$("#dm_lft_arm_part_1 img").height((35*actual_user_height)/100);
    
-   
-   //$("#dm_h_part_1").css("top",$("#dm_part_6 img").offset());
-   
-   //$("#dm_part_8 img").height((13.74*actual_user_height)/100);
 });
