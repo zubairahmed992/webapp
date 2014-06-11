316b67e4eb5b08719308eb0bb36412082d53a3da
waqas muddasir <swm@winCS>
2014-06-11 22:40:32 +0500

    shopify products list fetch action

--- src/LoveThatFit/ShopifyBundle/Controller/DefaultController.php
+++ src/LoveThatFit/ShopifyBundle/Controller/DefaultController.php
@@ -9,27 +9,41 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    #   temp-code = '8d9e6d19e48e3e002af09b19ea016081' 
+
+    #   $api_key = '1464612a653803d72f4d6d998e59d785';
+    #    $shared_secret = '497d438714a4fc95f688850fc476caa5';
+    #    $shop_domain = 'lovethatfit-2.myshopify.com';    
+    #    $access_token = '019860835bfd8ac1c5259702c5b953a9';
+    
    public function indexAction()
     {
         $api_key='1464612a653803d72f4d6d998e59d785';
         $shop_domain = 'lovethatfit-2.myshopify.com';
-        //$str = \sandeepshetty\shopify_api\install_url($shop_domain, $api_key);
-        $str = \sandeepshetty\shopify_api\permission_url($shop_domain, $api_key, array('read_products'));
-        //return $this->redirect('https://'.$shop_domain.'/admin/oauth/authorize?client_id='.$api_key);
+        
+        $str = \sandeepshetty\shopify_api\permission_url($shop_domain, $api_key, array('read_products'));        
         return $this->redirect($str);
-        
-        
-        
-        $shared_secret='497d438714a4fc95f688850fc476caa5';
-        $access_token = \sandeepshetty\shopify_api\oauth_access_token($shop_domain, $api_key, $shared_secret, $api_key);
-        $str+=' you are da real MVP';
-        
-        return $this->render('LoveThatFitShopifyBundle:Default:index.html.twig', array('data'=> $str));
     }
     
      public function grantedAction()
     {
-         $params = $this->getRequest()->request->all();
-         return new Response(json_encode($params));
+         $api_key = '1464612a653803d72f4d6d998e59d785';
+         $shared_secret = '497d438714a4fc95f688850fc476caa5';
+         $code = $this->getRequest()->query->get('code');          
+         $shop_domain = $this->getRequest()->query->get('shop');        
+         $access_token = \sandeepshetty\shopify_api\oauth_access_token($shop_domain, $api_key, $shared_secret, $code);
+         return new Response('code :  ' .$code .'   access token: '. $access_token);
+     }
+
+     public function productsAction()
+    {
+        $api_key = '1464612a653803d72f4d6d998e59d785';
+        $shared_secret = '497d438714a4fc95f688850fc476caa5';
+        $shop_domain = 'lovethatfit-2.myshopify.com';    
+        $access_token = '019860835bfd8ac1c5259702c5b953a9';
+        
+        $shopify = \sandeepshetty\shopify_api\client($shop_domain, $access_token, $api_key, $shared_secret);
+        $products = $shopify('GET', '/admin/products.json', array('published_status'=>'published'));
+        return new Response(json_encode($products));
      }
 }
