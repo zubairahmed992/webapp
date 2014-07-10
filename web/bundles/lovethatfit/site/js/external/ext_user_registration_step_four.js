2c25e09e26f069ca97619f7175ad537625795fc6
behlool <behlool@centricsource.com>
2014-07-10 17:36:32 +0500

    Did changes in step 4

--- web/bundles/lovethatfit/site/js/external/ext_user_registration_step_four.js
+++ web/bundles/lovethatfit/site/js/external/ext_user_registration_step_four.js
@@ -581,6 +581,7 @@ function call_settings(responseText, statusText, xhr, $form){
         var photo_file_name = $("#user_file").val();
         $("#inp_txt_file_name").val(photo_file_name);
         $("#play_area").removeClass("hide");
+        $(".ext_btns").hide();
         $("#how_it_works").removeClass("hide");
         $(".int_fitting_room").addClass("hide");
         $("#play_area").prepend('<div class="uploading_in_progress"></div>');
