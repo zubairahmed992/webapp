a40fc9ebbad4e1a75c91accd841a58b0f2dabbd5
suresh <suresh.kumar@centricsource.com>
2014-07-21 15:34:52 +0500

    Little changes

--- src/LoveThatFit/ShopifyBundle/Controller/DefaultController.php
+++ src/LoveThatFit/ShopifyBundle/Controller/DefaultController.php
@@ -155,12 +155,16 @@ class DefaultController extends Controller {
     }
 #---------------------------------Check Sku ---------------------------#
 public function checkSkuAction($sku=null){
-  $itemBySku = $this->get('admin.helper.productitem')->findItemBySku($sku);
+     $responseHeaders = new Response();
+        $responseHeaders->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+        $responseHeaders->set('Access-Control-Allow-Origin', '*');
+        $responseHeaders->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE OPTIONS');
+    $itemBySku = $this->get('admin.helper.productitem')->findItemBySku($sku);
     if ($itemBySku == null || empty($itemBySku)) {
-       return new response("one");
-    }   else{
-        return new response("two");
-    }
+       return new response(array('status' => 'success'), 200, array('Access-Control-Allow-Origin' => '*'));
+    }else{
+      return new response(array('status' => 'success'), 200, array('Access-Control-Allow-Origin' => '*'));
+    }   
 }
     
 
