658555a8fc576bb0ad0c8431af96d0b33957688b
suresh <suresh.kumar@centricsource.com>
2014-07-22 23:25:39 +0500

    check instalation  process of shopify

--- src/LoveThatFit/ShopifyBundle/Controller/DefaultController.php
+++ src/LoveThatFit/ShopifyBundle/Controller/DefaultController.php
@@ -11,9 +11,9 @@ class DefaultController extends Controller {
 
     public function installAction() {
         $app_specs = $this->get('shopify.helper')->appSpecs();
-        $shop_domain = 'lovethatfit-2.myshopify.com';
-        $str = \sandeepshetty\shopify_api\permission_url($shop_domain, $app_specs['api_key'], array('read_products', 'write_content', 'write_themes', 'read_customer'));
-        return new Response($str);
+        $shop_domain = 'bicycles-122.myshopify.com';
+        $str = \sandeepshetty\shopify_api\permission_url($shop_domain, $app_specs['api_key'], array('read_products','write_themes','write_content'));
+       //return new Response($str);
         return $this->redirect($str);
     }
 
@@ -25,24 +25,35 @@ class DefaultController extends Controller {
         $specs['shared_secret'] = $app_specs['shared_secret'];
 
         # to serve the shopify server app install request :--------------------
-        #$specs['temp_code'] = $this->getRequest()->query->get('code');          
-        #$specs['shop_domain'] = $this->getRequest()->query->get('shop');                 
-        #$specs['access_token'] = \sandeepshetty\shopify_api\oauth_access_token($specs['shop_domain'], $specs['api_key'], $specs['shared_secret'], $specs['temp_code']);
-        #~~~~~~>testing
-        $specs['temp_code'] = 'davinci_code';
-        $specs['shop_domain'] = 'lovethatfit-2.myshopify.com';
-        $specs['access_token'] = 'hush hush';
+        $specs['temp_code'] = $this->getRequest()->query->get('code');          
+        $specs['shop_domain'] = $this->getRequest()->query->get('shop');                 
+        $specs['access_token'] = \sandeepshetty\shopify_api\oauth_access_token($specs['shop_domain'], $specs['api_key'], $specs['shared_secret'], $specs['temp_code']);
+       //return new response(json_encode($specs));
+//1- access token ,shop domain in retailer table
+//        
+#~~~~~~>testing
+      // $specs['temp_code'] = '0de3b3fb61b001da821e81883b453537';
+      //  $specs['shop_domain'] = 'bicycles-122.myshopify.com';
+      // $specs['access_token'] = '3874b59ffd8c7c42bc18f28cc04d3e71';
         #----------------------------------------------------------------------
         # update in ltf database
-        $this->get('admin.helper.retailer')->updateRetailShopSpecs($specs);
-        return new Response(json_encode($specs));
+        
+        if($this->get('admin.helper.retailer')->updateRetailShopSpecs($specs)){
+          return new response($this->foo());
+        }else{
+            return new Response("Some thing went wrong!");
+        }
+        
     }
 
 #-------------------------------------->
 
-    public function fooAction() {
+    public function foo() {
+        
+         //return new response(json_encode($this->getShopMainTheme()));
         #$resp=json_encode($this->getShopProducts());
         $content = trim(preg_replace('/\s\s+/', '\n ', $this->getContent()));
+        
         $resp=json_encode($this->writeFile('snippets/foo.liquid', $content));
         return new Response($resp);
     }
@@ -57,20 +68,25 @@ class DefaultController extends Controller {
     #--------------------------------------------------------------- 
 
     public function userCheckAction(Request $request, $user_id, $sku) {
+       
         if ($user_id == null) {
             return $this->redirect($this->generateUrl('external_login'), 301);
         }
 
         $site_user = $this->get('admin.helper.retailer.site.user')->findByReferenceId($user_id);
-
+      
+       
         if (is_object($site_user)) {
+            
             $itemBySku = $this->get('admin.helper.productitem')->findItemBySku($sku);
-            if ($itemBySku == null || empty($itemBySku)) {
-                return new response("Product not found");
+           
+            if ($itemBySku == null || empty($itemBySku)|| !isset($itemBySku)){
+             return new response("Product not found"); 
             }
-            $this->get('user.helper.user')->getLoggedInById($site_user->getUser());
+             $this->get('user.helper.user')->getLoggedInById($site_user->getUser());
             return $this->redirect($this->generateUrl('inner_shopify_index', array('sku' => $sku, 'user_id' => $site_user->getId())), 301);
         } else {
+            
             //$retailer = $this->get('admin.helper.retailer')->find(1);
             $this->setNewUserSession($user_id, $sku);
             return $this->redirect($this->generateUrl('external_login'), 301);
@@ -84,26 +100,16 @@ class DefaultController extends Controller {
             'sku' => $sku));
     }
 
-    // ------------------Create HTML File ----------------------------
-    /*
-      $stringData = "<p>whatever you want inside the html file</p>";
-      $fullPath =  "fileName.html";
-      $response = new Response();
-      $response->headers->set('Content-Type', 'text/csv');
-      $response->headers->set('Content-Disposition', 'attachment;filename="'.$fullPath);
-      $response->setContent($stringData);
-
-     * return $response;
-     */
-
-
+  
 
     #~~~~~~~~~~~~~~~~~~~~~~ PRIVATES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
 
     private function getShopMainTheme() {
         $themes = $this->getShopThemes();
+        $th_array=json_decode($themes, true);
+        
         $main_theme = null;
-        foreach ($themes as $t) {
+        foreach ($th_array['themes'] as $t) {
             $main_theme = $t['role'] == 'main' ? $t : $main_theme;
         }
         return $main_theme;
@@ -112,7 +118,7 @@ class DefaultController extends Controller {
     #--------------------------------------->
 
     private function getShopThemes() {
-        $shopify = $this->getShopifyObject();
+      $shopify = $this->getShopifyObject();
         $themes = $shopify('GET', '/admin/themes.json');
         return $themes;
     }
@@ -129,8 +135,11 @@ class DefaultController extends Controller {
 
     private function getShopifyObject() {
         $app_specs = $this->get('shopify.helper')->appSpecs();
-        $access_token = '8b14eb6efcf7c5fa7b0c76e9b329d06e';
-        $shop_domain = 'lovethatfit-2.myshopify.com';
+       
+        //$access_token = '8b14eb6efcf7c5fa7b0c76e9b329d06e';
+       $access_token='f4042684c617709540ed837ce30aea98';
+        
+        $shop_domain = 'bicycles-122.myshopify.com';
         $shopify = \sandeepshetty\shopify_api\client($shop_domain, $access_token, $app_specs['api_key'], $app_specs['shared_secret']);
         return $shopify;
     }
@@ -166,10 +175,8 @@ public function checkSkuAction($sku=null){
     
 
 #-----------------------------------------------------------------------
-
-    private function getContent() {
-
-        return "<style>
+  private function getContent() {
+       return "<style>
 .full_screen {text-align:center;width:100%;height:100%;position:fixed;left:0;top:0;z-index:80000; background:url({{ 'trans_dot.png' | asset_url }}) 0 0 repeat;display:none;}
 .full_screen .inner_div {width:814px;height:584px;margin:0 auto;margin-top:36px;overflow:hidden;}
 .full_screen img {float:left;}
