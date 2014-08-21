ba8d021f8531b905492202b63634a8dfc2bdb8c8
Azeem <azeem.akram@centricsource.com>
2014-08-21 21:21:42 +0500

    Retailer order track and retailer order item track helper in external site bundle

---
+++ src/LoveThatFit/ExternalSiteBundle/Entity/RetailerOrderTrackHelper.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace LoveThatFit\ExternalSiteBundle\Entity;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Yaml\Parser;
+use LoveThatFit\ExternalSiteBundle\Entity\RetailerOrderTrack;
+
+class RetailerOrderTrackHelper {
+
+    protected $dispatcher;
+
+    /**
+     * @var EntityManager 
+     */
+    protected $em;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repo;
+
+    /**
+     * @var string
+     */
+    protected $class;
+    private $container;
+
+    public function __construct(EventDispatcherInterface $dispatcher, EntityManager $em, $class, Container $container)
+    {
+        $this->container = $container;
+        $this->dispatcher = $dispatcher;
+        $this->em = $em;
+        $this->class = $class;
+        $this->repo = $em->getRepository($class);
+        
+    }
+
+    //---------------------------------------------------------------------   
+
+    public function createNew() {
+        $class = $this->class;
+        $retailer_order_track = new $class();
+        return $retailer_order_track;
+    }
+
+//-------------------------------------------------------
+
+    
+    
+    public function save() {
+        
+    }
+
+    //-------------------------------------------------------
+
+    public function update() {
+
+        
+    }
+
+//-------------------------------------------------------
+
+    
+  
+
+//-------------------------------------------------------
+    
+    public function delete() {
+
+      
+    }
+    
+//-------------------------------------------------------
+
+ public function find($id) {
+        return $this->repo->find($id);
+    }
+#-----------------------------------------------------
+  public function findAll(){
+  return $this->repo->findAll();      
+    }
+
+   
+}
