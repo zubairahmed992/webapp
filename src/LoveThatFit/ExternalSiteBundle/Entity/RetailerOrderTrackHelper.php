716c72c0475521c736f5f2db6eb11c63994c936b
Azeem <azeem.akram@centricsource.com>
2014-08-28 22:45:30 +0500

    Retailer order item track save functionality to retailer order item

--- src/LoveThatFit/ExternalSiteBundle/Entity/RetailerOrderTrackHelper.php
+++ src/LoveThatFit/ExternalSiteBundle/Entity/RetailerOrderTrackHelper.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Yaml\Parser;
 use LoveThatFit\ExternalSiteBundle\Entity\RetailerOrderTrack;
 use LoveThatFit\ExternalSiteBundle\Entity\RetailerOrderItemTrack;
-use LoveThatFit\SiteBundle\AvgAlgorithm;
+
 
 class RetailerOrderTrackHelper {
 
@@ -58,7 +58,7 @@ class RetailerOrderTrackHelper {
         $retailer = $this->container->get('admin.helper.retailer')->findRetailerBySite($retailer_order['referring_site']);
         if (!$retailer) return 'retiler not found!';
         
-        $site_user = $this->container->get('admin.helper.retailer.site.user')->getByReferenceId($retailer_order['customer']['id'], $retailer->getId());
+        $site_user = $this->container->get('admin.helper.retailer.site.user')->findByReferenceId($retailer_order['customer']['id'], $retailer->getId());
         if (!$site_user) return 'user not found!';
         
         $user = $site_user->getUser(); #????? ['user']['id']
@@ -66,7 +66,7 @@ class RetailerOrderTrackHelper {
         
         $entity = new RetailerOrderTrack();
         $entity->setCartToken($retailer_order['cart_token']);
-        #$entity->setClosedAt(new \DateTime('now')); #get from json
+        //$entity->setClosedAt($retailer_order['created_at']); #get from json
         $entity->setToken($retailer_order['token']);   
         $entity->setOrderStatus($retailer_order['fulfillment_status']);
         $entity->setOrderReferenceId($retailer_order['id']);
@@ -75,52 +75,11 @@ class RetailerOrderTrackHelper {
         $entity->setRetailer($retailer);
         $entity->setUser($user);
         $this->save($entity);
-        
-        return $this->saveRetailerOrderItemTrack($entity,$user, $retailer_order);
+        $this->container->get('retailer.order.item.track.helper')->saveRetailerOrderItem($entity,$user, $retailer_order);
+        //return $this->saveRetailerOrderItemTrack($entity,$user, $retailer_order);
     }
   
-  Public function saveRetailerOrderItemTrack($entity, $user, $retailer_order) {
-        
-        $order_item = new RetailerOrderItemTrack();
-        foreach ($retailer_order['line_items'] as $retailer_item) {
-            $order_item->setSku($retailer_item['sku']);
-            
-            $product_item = $this->container->get('admin.helper.productitem')->findItemBySku($retailer_item['sku']);
-            $product_size = $product_item->getProductSize();
-            $product = $product_item->getProduct();
-            #------feedback 
-            $comp = new AvgAlgorithm($user, $product);
-            $fb = $comp->getSizeFeedBack($product_size);
-            #------------ recommendation 
-            if ($fb && array_key_exists('recommendation', $fb) && $fb['recommendation']) {
-                $recommended_size = $fb['recommendation']['title'];
-                $recommended_index = $fb['recommendation']['fit_index'];
-            } else {
-                if ($fb['feedback']['fits']) {
-                    $recommended_size = $fb['feedback']['title'];
-                    $recommended_index = $fb['feedback']['fit_index'];
-                }
-            }
-            #------------------------------------            
-            $order_item->setPurchasedFitIndex($fb['feedback']['fit_index']);
-            $order_item->setPurchasedFitSize($fb['feedback']['title']);
-            $order_item->setRecommendedFitIndex($recommended_index);
-            $order_item->setRecommendedFitSize($recommended_size);
-            #-----------------------------------
-            $user_tried = $this->container->get('site.helper.usertryitemhistory')->countUserItemTryHistory($user, $product, $product_item);
-            if ($user_tried > 0) {
-                $order_item->setTriedOn(1);
-            } else {
-                $order_item->setTriedOn(0);
-            }
-            
-            $order_item->setRetailerOrderTrack($entity);
-            $order_item->setProductItems($product_item);
-            $this->em->persist($order_item);
-            $this->em->flush();
-        }
-        return $order_item;
-    }
+   
 
 //-------------------------------------------------------
 
