c7e9d4781ded5ea998bf483c2ab90e505c1333a2
suresh <suresh.kumar@centricsource.com>
2014-09-11 23:19:48 +0500

    Javascript moved in custom js of admin

--- web/bundles/lovethatfit/admin/js/custom.js
+++ web/bundles/lovethatfit/admin/js/custom.js
@@ -1,3 +1,96 @@
 // NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
 // IT'S ALL JUST JUNK FOR OUR DOCS!
 // ++++++++++++++++++++++++++++++++++++++++++
+
+
+//---------------------------------------------------------------------------
+function splitStr(str){
+  var myStr=str.split("_");   
+  var flag="1";
+   if((myStr[0]) && (typeof myStr[0] != 'undefined')){
+       flag+="1";
+   } if((myStr[1]) && (typeof myStr[1] != 'undefined')){
+       flag+="1";
+   }
+    if((myStr[2]) && (typeof myStr[2] != 'undefined')){
+       flag+="1";
+   }
+  return flag; 
+ }
+ // Multiple file uploading method for  producs Item--------------------------#
+$(document).ready(function(){
+$("#file").change(function(){
+var src=$("#file").val();
+var product_id=$('#product_id').val();
+if(src!="")
+{
+formdata= new FormData();
+var numfiles=this.files.length;
+var i, file, progress, size;
+for(i=0;i<numfiles;i++)
+{
+file = this.files[i];
+
+if(splitStr(file.name)!="1111"){
+    alert("Invalid Image Format");
+    return false;
+}
+size = this.files[i].size;
+name = this.files[i].name;
+if (!!file.type.match(/image.*/))
+{
+if((Math.round(size))<=(1024*1024))
+{
+var reader = new FileReader();
+reader.readAsDataURL(file);
+$("#preview").show();
+$('#preview').html("");
+reader.onloadend = function(e){
+var image = $('<img>').attr('src',e.target.result);
+$(image).appendTo('#preview');
+};
+formdata.append("file[]", file);
+if(i==(numfiles-1))
+{
+    formdata.append("product_id", product_id);
+$("#info").html("wait a moment to complete upload");
+$.ajax({
+	url: "{{path('admin_product_detail_multiple_images_uploading')}}",
+	type: "POST",
+	data: formdata,
+	processData: false,
+	contentType: false,
+	success: function(res){
+	if(res!="0")
+	$("#info").html("Successfully Uploaded");
+	else
+	$("#info").html("Error in upload. Retry");
+	}
+	});
+}
+}
+else
+{
+$("#info").html(name+"Size limit exceeded");
+$("#preview").hide();
+return;
+}
+}
+else
+{
+$("#info").html(name+"Not image file");
+$("#preview").hide();
+return;
+}
+}
+}
+else
+{
+$("#info").html("Select an image file");
+$("#preview").hide();
+return;
+}
+return false;
+});
+});
+//--------------------End of Multiple file uplaoding method---------------------#
