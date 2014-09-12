1ca05d8bf618aabc300f7f69e2033058bcb18dae
waqas muddasir <swm@winCS>
2014-09-12 23:08:43 +0500

    masked marker inspection interface

---
+++ src/LoveThatFit/AdminBundle/Controller/MaskMarkerInspectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LoveThatFit\AdminBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Yaml\Parser;
+
+
+class MaskMarkerInspectController extends Controller {
+
+    public function indexAction(){
+        $form=$this->createForm(new \LoveThatFit\UserBundle\Form\Type\UserDropdownType());
+        return $this->render('LoveThatFitAdminBundle:MaskMarkerInspect:index.html.twig', array(
+                    'form' => $form->createView(),                    
+                ));
+    }   
+    
+    public function userAction($id){
+        $user = $this->get('user.helper.user')->find($id);
+        $mm_specs=$this->getMaskedMarkerSpecs();
+        $ub_specs=$user->getMeasurement()->getArray();
+        #return new Response(json_encode($specs['masked_marker']));
+        return $this->render('LoveThatFitAdminBundle:MaskMarkerInspect:_summary.html.twig', array(
+                    'user' => $user,                    
+                    'specs'=>$mm_specs,
+                    'body_measurement'=>$ub_specs,
+                ));
+    }   
+    
+    private function getMaskedMarkerSpecs() {
+        $yaml = new Parser();
+        return $yaml->parse(file_get_contents('../src/LoveThatFit/UserBundle/Resources/config/mask_marker.yml'));
+    }
+}
---
+++ src/LoveThatFit/UserBundle/Form/Type/UserDropdownType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LoveThatFit\UserBundle\Form\Type;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UserDropdownType extends AbstractType
+{   
+    
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder ->add('User', 'entity', array(
+                    'class' => 'LoveThatFitUserBundle:User',
+                    'expanded' => false,
+                    'multiple' => false,
+                    'required' => false,
+                    'property' => 'email',
+                    'empty_value' => 'Select User'
+                ));
+    }
+    public function getName()
+    {
+        return 'user';
+    }
+
+    
+}
+
+?>
