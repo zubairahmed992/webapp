8cf9fcbd8d6bcf21b10c038f513298a4ae2c45ff
suresh <suresh.kumar@centricsource.com>
2014-10-29 16:34:23 +0500

    General Measurement yml n helper

---
+++ src/LoveThatFit/AdminBundle/Entity/GeneralMeasurementsHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LoveThatFit\AdminBundle\Entity;
+
+use Symfony\Component\Yaml\Parser;
+
+class GeneralMeasurementsHelper {
+
+    protected $conf;
+
+    public function __construct() {
+        $conf_yml = new Parser();
+        $this->conf = $conf_yml->parse(file_get_contents('../app/config/general_measurements.yml'));
+    }
+#---------------- ------------------------------------------#
+
+    public function getManGeneralMeasurement() {
+        return $this->conf["general_measurements"]["man"];
+    }
+
+#---------------------------------------------------------------------------
+ public function getMeasurementByNeck($neck_size) {
+        $manMeasurements = $this->getManGeneralMeasurement();
+        $averageArray = array();
+        foreach ($manMeasurements as $key => $value) {
+            if ($value['neck'] == $neck_size) {
+                $averageArray['key'] = $key;
+            }
+        }
+
+        return $this->getmeasurementByKey($averageArray['key']);
+    }
+
+#----------------------------------------------------------------------------
+    public function getmeasurementByKey($key) {
+        return $this->conf["general_measurements"]["man"][$key];
+    }
+
+}
