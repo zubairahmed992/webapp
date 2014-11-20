ed9ca7b060fd5ddfc91fffc01cab6b58eb5751ab
waqas muddasir <swm@winCS>
2014-11-20 19:04:22 +0500

    reg-Step-2: size chart additional size titles removed

--- src/LoveThatFit/UserBundle/Controller/AjaxController.php
+++ src/LoveThatFit/UserBundle/Controller/AjaxController.php
@@ -18,73 +18,12 @@ class AjaxController extends Controller {
                 ->getRepository('LoveThatFitAdminBundle:SizeChart')
                 ->findByBrandGenderTargetBodyType($brand_id, $gender, $target,$body_type);
         //$response = new Response(json_encode($sizes));
-                   $response = new Response(json_encode($this->setSizeTitles($sizes)));
+                   $response = new Response(json_encode($sizes));
                 $response->headers->set('Content-Type', 'text/html');          
                 return $response;
     }
     
-    public function setSizeTitles($sizes)
-{ $new_sizes=array();
-    $new_key='';
-    $str="";
-    foreach ($sizes as $value) {
-        
-        switch ($value['title']){
-            case "0":
-                $new_key="XXS : ".$value['title'];
-                break;
-            case "1":
-                $new_key="XS : ".$value['title'];
-                break;
-            case "2":
-                $new_key="XS : ".$value['title'];
-                break;
-            case "4":
-                $new_key="S : ".$value['title'];
-                break;
-            case "6":
-                $new_key="S : ".$value['title'];
-                break;
-            case "8":
-                $new_key="M : ".$value['title'];
-                break;
-            case "10":
-                $new_key="M : ".$value['title'];
-                break;
-            case "12":
-                $new_key="L : ".$value['title'];
-                break;
-            case "14":
-                $new_key="L : ".$value['title'];
-                break;
-            case "16":
-                $new_key="XL : ".$value['title'];
-                break;
-            case "18":
-                $new_key="XL : ".$value['title'];
-                break;
-            case "20":
-                $new_key="XXL : ".$value['title'];
-                break;
-            case "22":
-                $new_key="XXL : ".$value['title'];
-                break;
-            case "24":
-                $new_key="XXL : ".$value['title'];
-                break;
-            case "26":
-                $new_key="XXL : ".$value['title'];
-                break;
-            case "28":
-                $new_key="XXL : ".$value['title'];
-                break;
-        }
-        
-        array_push($new_sizes, array("title"=>$new_key, "id"=>$value['id']));
-        
-    }
-    return $new_sizes;
-}
+   
     #--!!!!!This should removed when profile setting update !!!!---------#
     public function brandSizeChartByJSONAction($brand_id, $gender, $target) {
         
