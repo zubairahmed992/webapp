056d0bed2339b6a33a2bdb51ba0b0c2bd8fbff3b
Azeem <azeem.akram@centricsource.com>
2014-12-03 18:05:27 +0500

    Bits changed in user in admin for refactoring

--- src/LoveThatFit/AdminBundle/Controller/UserController.php
+++ src/LoveThatFit/AdminBundle/Controller/UserController.php
@@ -14,25 +14,21 @@ use LoveThatFit\AdminBundle\Form\Type\RetailerSiteUserType;
 
 class UserController extends Controller {
 
-    //------------------------------------------------------------------------------------------
-
-
+    //--------------------------User List-------------------------------------------------------------
     public function indexAction($page_number, $sort = 'id') {
         $size_with_pagination = $this->get('user.helper.user')->getListWithPagination($page_number, $sort);
         return $this->render('LoveThatFitAdminBundle:User:index.html.twig', array('pagination' => $size_with_pagination, 'searchform' => $this->userSearchFrom()->createView()));
     }
 
+    //-------------------------Show user detail-------------------------------------------------------
     public function showAction($id) {
-        $specs = $this->get('user.helper.user')->findWithSpecs($id);
-        $entity = $specs['entity'];
+        $entity = $this->get('user.helper.user')->find($id);        
         $user_limit = $this->get('user.helper.user')->getRecordsCountWithCurrentUserLimit($id);
         $page_number = ceil($this->get('admin.helper.utility')->getPageNumber($user_limit[0]['id']));
-        if ($page_number == 0) {
-            $page_number = 1;
-        }
-        if ($specs['success'] == false) {
-            $this->get('session')->setFlash($specs['message_type'], $specs['message']);
-        }
+        $page_number=$page_number==0?1:$page_number;        
+        if(!$entity){        
+            $this->get('session')->setFlash('warning', 'User not found!');
+        }     
         return $this->render('LoveThatFitAdminBundle:User:show.html.twig', array(
                     'user' => $entity,
                     'page_number' => $page_number,
@@ -42,6 +38,7 @@ class UserController extends Controller {
                 ));
     }
 
+    //--------------------------User Serach------------------------------------------------------------
     public function searchAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $data = $request->request->all();
@@ -85,7 +82,167 @@ class UserController extends Controller {
         }
     }
 
-//------------------------------------------------------------------------------------------
+    //-------------------------------Edit User-----------------------------------------------------------
+    public function editAction($id) {
+        $entity = $this->get('user.helper.user')->find($id);
+        $measurement = $entity->getMeasurement();
+        $measurementForm = $this->createForm(new UserMeasurementType(), $measurement);
+        $userForm=$this->createForm(new UserProfileSettingsType(), $entity);
+        return $this->render('LoveThatFitAdminBundle:User:edit.html.twig', array(
+                    'form' => $measurementForm->createView(),
+                    'userform' => $userForm->createView(),
+                    'measurement' => $measurement,
+                    'entity' => $entity,
+                ));
+    }
+    
+    //--------------------------Update User----------------------------------------------------------------
+    public function updateAction($id) {
+        $entity = $this->get('user.helper.user')->find($id);
+        $measurement = $entity->getMeasurement();
+        $measurementForm = $this->createForm(new UserMeasurementType(), $measurement);
+        $measurementForm->bind($this->getRequest());
+        $measurement->setUpdatedAt(new \DateTime('now'));
+        $this->get('user.helper.measurement')->saveMeasurement($measurement);
+        $this->get('session')->setFlash('success', 'Updated Successfuly');
+        $userForm=$this->createForm(new UserProfileSettingsType(), $entity);
+        return $this->render('LoveThatFitAdminBundle:User:edit.html.twig', array(
+                    'form' => $measurementForm->createView(),
+                    'userform' => $userForm->createView(),
+                    'measurement' => $measurement,
+                    'entity' => $entity,
+                ));
+    }    
+    
+    //-----------------------------Delete User-----------------------------------------------------------
+   public  function  deleteAction($id)
+    {
+          try {
+            $message_array = $this->get('user.helper.user')->delete($id);
+            $this->get('session')->setFlash($message_array['message_type'], $message_array['message']);
+
+            return $this->redirect($this->generateUrl('admin_users'));
+        } catch (\Doctrine\DBAL\DBALException $e) {
+
+            $this->get('session')->setFlash('warning', 'This Size cannot be deleted!');
+            return $this->redirect($this->getRequest()->headers->get('referer'));
+        }
+    }
+    
+//----------------------User profile update-------------------------------------------------------------
+    public function updateUserProfileAction($id)
+    {
+        $entity = $this->get('user.helper.user')->find($id);
+        $measurement = $entity->getMeasurement();
+        $measurementForm = $this->createForm(new UserMeasurementType(), $measurement);
+        $userForm=$this->createForm(new UserProfileSettingsType(), $entity);
+        $userForm->bind($this->getRequest());       
+        $this->get('user.helper.user')->saveUser($entity);
+        $this->get('session')->setFlash('success', 'Updated Successfuly');
+        return $this->render('LoveThatFitAdminBundle:User:edit.html.twig', array(
+                    'form' => $measurementForm->createView(),
+                    'userform' => $userForm->createView(),
+                    'measurement' => $measurement,
+                    'entity' => $entity,
+                ));
+    }
+    
+    //-------------------------------------Compare User Form--------------------------------------------
+    public function comapareUserAction()
+    {
+        $form=$this->createForm(new MannequinTestType());
+        return $this->render('LoveThatFitAdminBundle:User:compare.html.twig', array(
+                    'form' => $form->createView(),                   
+                ));
+        
+    }
+    //---------------------Compare User Mannequin-------------------------------------------------------
+    
+    public function comapareUserSizeAction(Request $request)
+    {        
+        $form=$this->createForm(new MannequinTestType());                
+        $data = $request->request->all();
+        $email = $data['user']['User'];
+        $entity = $this->get('user.helper.user')->find($email);       
+        $manequin_size=$this->get('admin.helper.user.mannequin')->userMannequin($entity);        
+        return new Response(json_encode($manequin_size));
+        return $this->render('LoveThatFitAdminBundle:User:compare.html.twig', array(
+                    'form' => $form->createView(),                   
+         ));
+    }
+    
+  //-----------------------Add New Retailer Site user---------------------------------------------------  
+   public function newRetailerSiteUserAction($id)
+   {
+       $entity = $this->get('user.helper.user')->find($id);
+       $RetailerSiteUser = $this->get('admin.helper.retailer.site.user')->createNew();
+       $RetailerSiteUserForm=$this->createForm(new RetailerSiteUserType('add'), $RetailerSiteUser);
+       return $this->render('LoveThatFitAdminBundle:User:new_retailer_site_user.html.twig', array(
+                    'user' => $entity,                   
+                    'form' => $RetailerSiteUserForm->createView(),
+                ));
+
+   }
+  
+   
+   //-----------------------Create New Retailer Site user---------------------------------------------------
+   public function createRetailerSiteUserAction(Request $request,$id)
+   {        
+       $data = $request->request->all();    
+       $retailerId=$data['retailer_site_user']['Retailer'];
+       $user_reference_id=$data['retailer_site_user']['user_reference_id'];
+       $retailer=$this->get('admin.helper.retailer')->find($retailerId);         
+       $user= $this->get('user.helper.user')->find($id);
+       $this->get('admin.helper.retailer.site.user')->addNew($user, $user_reference_id, $retailer);
+        return $this->redirect($this->generateUrl('admin_user_detail_show', array('id' => $user->getId())));
+   }
+   
+   //-------------------------------Edit Retailer Site Users--------------------------------------------
+   public function editRetailerSiteUserAction($user_id,$id)
+   {
+       $entity=$this->get('admin.helper.retailer.site.user')->find($id);
+       if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Retailer Site User.');
+        }
+        $user=$this->get('user.helper.user')->find($user_id);
+        $form = $this->createForm(new RetailerSiteUserType('edit'), $entity);
+        return $this->render('LoveThatFitAdminBundle:User:edit_retailer_site_user.html.twig', array(
+                    'user' => $user,                   
+                    'form' => $form->createView(),
+                    'entity'=>$entity, 
+                ));
+   }
+   //-------------------------------Update Retailer Site Users--------------------------------------------
+   public function updateRetailerSiteUserAction(Request $request,$user_id,$id)
+   {
+       $data = $request->request->all();    
+       $retailerId=$data['retailer_site_user']['Retailer'];
+       $user_reference_id=$data['retailer_site_user']['user_reference_id'];
+       $retailer=$this->get('admin.helper.retailer')->find($retailerId);       
+       $entity= $this->get('user.helper.user')->find($user_id);
+       $retailerSiteUser=$this->get('admin.helper.retailer.site.user')->find($id);
+       $this->get('admin.helper.retailer.site.user')->update($retailerSiteUser,$retailer,$entity,$user_reference_id);
+        return $this->redirect($this->generateUrl('admin_user_detail_show', array('id' => $entity->getId())));
+   }
+   
+   //----------------------------Delete Retailer Site Users---------------------------------------------
+    public function deleteRetailerSiteUserAction($id)
+    {
+        try {
+            $message_array = $this->get('admin.helper.retailer.site.user')->delete($id);
+            $this->get('session')->setFlash($message_array['message_type'], $message_array['message']);
+
+            return $this->redirect($this->generateUrl('admin_users'));
+        } catch (\Doctrine\DBAL\DBALException $e) {
+
+            $this->get('session')->setFlash('warning', 'This Site user cannot be deleted!');
+            return $this->redirect($this->getRequest()->headers->get('referer'));
+        }
+    }
+    
+    
+    
+    //-------------------------------User Search Form-----------------------------------------------------------
 
     private function userSearchFrom() {
         $user = new User();
@@ -105,176 +262,4 @@ class UserController extends Controller {
                         ))
                         ->getForm();
     }
-
-    //------------------------------------------------------------------------------------------
-    public function editAction($id) {
-        $entity = $this->get('user.helper.user')->find($id);
-        $measurement = $entity->getMeasurement();
-        $measurementForm = $this->createForm(new UserMeasurementType(), $measurement);
-        $userForm=$this->createForm(new UserProfileSettingsType(), $entity);
-        return $this->render('LoveThatFitAdminBundle:user:edit.html.twig', array(
-                    'form' => $measurementForm->createView(),
-                    'userform' => $userForm->createView(),
-                    'measurement' => $measurement,
-                    'entity' => $entity,
-                ));
-    }
-    //------------------------------------------------------------------------------------------
-    
-    //-----------------------------Delete User-----------------------------------------------------------
-   public  function  deleteAction($id)
-    {
-          try {
-            $message_array = $this->get('user.helper.user')->delete($id);
-            $this->get('session')->setFlash($message_array['message_type'], $message_array['message']);
-
-            return $this->redirect($this->generateUrl('admin_users'));
-        } catch (\Doctrine\DBAL\DBALException $e) {
-
-            $this->get('session')->setFlash('warning', 'This Size cannot be deleted!');
-            return $this->redirect($this->getRequest()->headers->get('referer'));
-        }
-    }
-    
-    
-    
-//------------------------------------------------------------------------------------------
-    public function updateAction($id) {
-        $entity = $this->get('user.helper.user')->find($id);
-        $measurement = $entity->getMeasurement();
-        $measurementForm = $this->createForm(new UserMeasurementType(), $measurement);
-        $measurementForm->bind($this->getRequest());
-        $measurement->setUpdatedAt(new \DateTime('now'));
-        $this->get('user.helper.measurement')->saveMeasurement($measurement);
-        $this->get('session')->setFlash('success', 'Updated Successfuly');
-        $userForm=$this->createForm(new UserProfileSettingsType(), $entity);
-        return $this->render('LoveThatFitAdminBundle:user:edit.html.twig', array(
-                    'form' => $measurementForm->createView(),
-                    'userform' => $userForm->createView(),
-                    'measurement' => $measurement,
-                    'entity' => $entity,
-                ));
-    }
-    
-    public function updateUserProfileAction($id)
-    {
-        $entity = $this->get('user.helper.user')->find($id);
-        $measurement = $entity->getMeasurement();
-        $measurementForm = $this->createForm(new UserMeasurementType(), $measurement);
-        $userForm=$this->createForm(new UserProfileSettingsType(), $entity);
-        $userForm->bind($this->getRequest());       
-        $this->get('user.helper.user')->saveUser($entity);
-        $this->get('session')->setFlash('success', 'Updated Successfuly');
-        return $this->render('LoveThatFitAdminBundle:user:edit.html.twig', array(
-                    'form' => $measurementForm->createView(),
-                    'userform' => $userForm->createView(),
-                    'measurement' => $measurement,
-                    'entity' => $entity,
-                ));
-    }
-    
-    
-    public function comapareUserAction()
-    {
-        $form=$this->createForm(new MannequinTestType());
-        return $this->render('LoveThatFitAdminBundle:user:compare.html.twig', array(
-                    'form' => $form->createView(),                   
-                ));
-        
-    }
-    
-    public function comapareUserSizeAction(Request $request)
-    {        
-        $form=$this->createForm(new MannequinTestType());                
-        $data = $request->request->all();
-        $email = $data['user']['User'];
-        $entity = $this->get('user.helper.user')->find($email);       
-        $manequin_size=$this->get('admin.helper.user.mannequin')->userMannequin($entity);        
-        return new Response(json_encode($manequin_size));
-        return $this->render('LoveThatFitAdminBundle:user:compare.html.twig', array(
-                    'form' => $form->createView(),                   
-         ));
-    }
-    
-    
-   public function newRetailerSiteUserAction($id)
-   {
-       $entity = $this->get('user.helper.user')->find($id);
-       $RetailerSiteUser = $this->get('admin.helper.retailer.site.user')->createNew();
-       $RetailerSiteUserForm=$this->createForm(new RetailerSiteUserType('add'), $RetailerSiteUser);
-       return $this->render('LoveThatFitAdminBundle:User:new_retailer_site_user.html.twig', array(
-                    'user' => $entity,                   
-                    'form' => $RetailerSiteUserForm->createView(),
-                ));
-
-   }
-   
-   
-   public function createRetailerSiteUserAction(Request $request,$id)
-   {        
-       $data = $request->request->all();    
-       $retailerId=$data['retailer_site_user']['Retailer'];
-       $user_reference_id=$data['retailer_site_user']['user_reference_id'];
-       $retailer=$this->get('admin.helper.retailer')->find($retailerId);         
-       $user= $this->get('user.helper.user')->find($id);
-       $this->get('admin.helper.retailer.site.user')->addNew($user, $user_reference_id, $retailer);
-        return $this->redirect($this->generateUrl('admin_user_detail_show', array('id' => $user->getId())));
-   }
-   
-   
-   public function editRetailerSiteUserAction($user_id,$id)
-   {
-       $entity=$this->get('admin.helper.retailer.site.user')->find($id);
-       if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Retailer Site User.');
-        }
-        $user=$this->get('user.helper.user')->find($user_id);
-        $form = $this->createForm(new RetailerSiteUserType('edit'), $entity);
-        return $this->render('LoveThatFitAdminBundle:User:edit_retailer_site_user.html.twig', array(
-                    'user' => $user,                   
-                    'form' => $form->createView(),
-                    'entity'=>$entity, 
-                ));
-   }   
-   public function updateRetailerSiteUserAction(Request $request,$user_id,$id)
-   {
-       $data = $request->request->all();    
-       $retailerId=$data['retailer_site_user']['Retailer'];
-       $user_reference_id=$data['retailer_site_user']['user_reference_id'];
-       $retailer=$this->get('admin.helper.retailer')->find($retailerId);       
-       $entity= $this->get('user.helper.user')->find($user_id);
-       $retailerSiteUser=$this->get('admin.helper.retailer.site.user')->find($id);
-       $this->get('admin.helper.retailer.site.user')->update($retailerSiteUser,$retailer,$entity,$user_reference_id);
-        return $this->redirect($this->generateUrl('admin_user_detail_show', array('id' => $entity->getId())));
-   }
-   
-    public function deleteRetailerSiteUserAction($id)
-    {
-        try {
-            $message_array = $this->get('admin.helper.retailer.site.user')->delete($id);
-            $this->get('session')->setFlash($message_array['message_type'], $message_array['message']);
-
-            return $this->redirect($this->generateUrl('admin_users'));
-        } catch (\Doctrine\DBAL\DBALException $e) {
-
-            $this->get('session')->setFlash('warning', 'This Site user cannot be deleted!');
-            return $this->redirect($this->getRequest()->headers->get('referer'));
-        }
-    }
-    
-    
-    
-    public function tossAction($id) {
-
-        $entity_item = $this->get('admin.helper.productitem')->find($id);
-        if (!$entity_item) {
-            throw $this->createNotFoundException('Unable to find Product Item.');
-        }
-        $str = var_dump($entity_item->getImagePaths());
-        return new Response($str);
-    }
-    
-    
-   
-    
 }
