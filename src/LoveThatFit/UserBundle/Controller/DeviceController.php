00233fa10cb4b9ea1490879c58fda5708d1bfdcb
waqas muddasir <swm@winCS>
2015-01-16 17:56:48 +0500

    demo app login & services changed

--- src/LoveThatFit/UserBundle/Controller/DeviceController.php
+++ src/LoveThatFit/UserBundle/Controller/DeviceController.php
@@ -24,114 +24,16 @@ class DeviceController extends Controller {
         );
     }
     
-      #--------------------------Registration Step Four For Ipad
-   public function _editImageAction( $edit_type = null) {
-       $securityContext = $this->container->get('security.context');
-       
-        if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $id = $securityContext->getToken()->getUser()->getId();
-        } else{
-            return $this->redirect($this->generateUrl('device_browser_login'));
-        }
-       
-        $user = $this->get('user.helper.user')->find($id);
-        if (!$user) {
-            throw $this->createNotFoundException('Unable to find User.');
-        }
-        $measurement = $user->getMeasurement();
-        
-        $measurement_vertical_form = $this->createForm(new MeasurementVerticalPositionFormType(), $measurement);
-        $measurement_horizontal_form = $this->createForm(new MeasurementHorizantalPositionFormType(), $measurement);
-        $form = $this->createForm(new RegistrationStepFourType(), $user);
-        $measurement_form = $this->createForm(new MeasurementStepFourType(), $measurement);
-        $marker = $this->get('user.marker.helper')->getByUser($user);
-        #return new response($marker->getRectHeight());   
-        $edit_type=$edit_type==null?'registration':'fitting_room';
-        
-        return $this->render('LoveThatFitUserBundle:Device:ipad_stepfour.html.twig', array(
-                    'form' => $form->createView(),                    
-                    'measurement_form' => $measurement_form->createView(),                   
-                    'measurement_vertical_form' => $measurement_vertical_form->createView(),
-                    'measurement_horizontal_form' => $measurement_horizontal_form->createView(),
-                    'entity' => $user,
-                    'measurement' => $measurement,
-                    'edit_type' => $edit_type,
-                    'marker' => $marker,
-            ));
-   }
- 
-   
+      
     #---------------------------------------------------------------------------
     
-   public function editImageAction($edit_type=null) {
-       $securityContext = $this->container->get('security.context');
+   public function editImageAction($auth_token=null, $edit_type=null) {
+       $user = $this->get('webservice.helper.user')->findByAuthToken($auth_token);
+       if(!$user){
+           return new Response ('Authentication error');
+       }
        
-       if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $user = $securityContext->getToken()->getUser();
-        } else{
-            return $this->redirect($this->generateUrl('device_browser_login'));
-        }
-        $measurement = $user->getMeasurement();
-         $users=$this->get('user.helper.parent.child')->findByUser($user);
-        if($users)
-        {
-        if($users->getIsApproved()=='0')
-        {        // Rendering step four
-            $form = $this->createForm(new UserParentChildLinkType());
-            return $this->render('LoveThatFitUserBundle:Registration:user_parent_child.html.twig', array(
-                    'form' => $form->createView(),                    
-                    'entity' => $user,      
-                    'edit_type' => 'disaprove',
-                    'isapproved'=>0,
-                ));
-        }
-        if($users->getIsApproved()==NULL or $users->getIsApproved()==null or $users->getIsApproved()=='' )
-        {        // Rendering step four
-            $form = $this->createForm(new UserParentChildLinkType());
-            return $this->render('LoveThatFitUserBundle:Registration:user_parent_child.html.twig', array(
-                    'form' => $form->createView(),                    
-                    'entity' => $user,      
-                    'edit_type' => 'fitting_room',
-                    'isapproved'=>$users->getIsApproved(),
-                ));
-        }
-        
-        
-        if($users->getIsApproved()==1)
-        {        // Rendering step four
-          $measurement_vertical_form = $this->createForm(new MeasurementVerticalPositionFormType(), $measurement);
-        $measurement_horizontal_form = $this->createForm(new MeasurementHorizantalPositionFormType(), $measurement);
-        $form = $this->createForm(new RegistrationStepFourType(), $user);
-        $measurement_form = $this->createForm(new MeasurementStepFourType(), $measurement);
-        $marker = $this->get('user.marker.helper')->getByUser($user);
-        $default_marker = $this->get('user.marker.helper')->getDefaultValuesBaseOnBodyType($user);
-       // return new response(json_encode($default_marker));
-                
-        $edit_type=$edit_type==null?'registration':$edit_type;
-        
-        return $this->render('LoveThatFitUserBundle:Device:device_view.html.twig', array(
-                    'form' => $form->createView(),
-                    'form' => $form->createView(),
-                        'measurement_form' => $measurement_form->createView(),                   
-                    'measurement_vertical_form' => $measurement_vertical_form->createView(),
-                    'measurement_horizontal_form' => $measurement_horizontal_form->createView(),
-                    'entity' => $user,
-                    'measurement' => $measurement,
-                    'edit_type' => $edit_type,
-                    'marker' => $marker,
-                    'default_marker' => $default_marker,
-            ));
-        }        
-        }elseif(!$users  and $user->getAge()<15 and $user->isApproved==NULL){        
-            $form = $this->createForm(new UserParentChildLinkType());
-            return $this->render('LoveThatFitUserBundle:Registration:user_parent_child.html.twig', array(
-                    'form' => $form->createView(),                    
-                    'entity' => $user,      
-                    'edit_type' => 'registration',
-                    'isapproved'=>$user->isApproved,
-                ));
-        }else
-        {        
+        $measurement = $user->getMeasurement();                
         $measurement_vertical_form = $this->createForm(new MeasurementVerticalPositionFormType(), $measurement);
         $measurement_horizontal_form = $this->createForm(new MeasurementHorizantalPositionFormType(), $measurement);
         $form = $this->createForm(new RegistrationStepFourType(), $user);
@@ -153,22 +55,19 @@ class DeviceController extends Controller {
                     'marker' => $marker,
                     'default_marker' => $default_marker,
             ));
-    }
+    
 }
 #----------------------------------------------------------------------------
    public function editImageAuthAction() {
-        $handle = fopen('php://input', 'r');
-        $jsonInput = fgets($handle);
-        $request_array = json_decode($jsonInput, true);
-        $token = $request_array['authTokenWebService'];
-
-        $user = $this->get('user.helper.user')->findByAuthToken($token);
-        if ($token && $user) {
-            $this->get('user.helper.user')->getLoggedIn($user);
-            return $this->redirect($this->generateUrl('device_browser_image_edit_web'));
-        } else {
-            return new Response('Unable to Authenticate.. ');
+        $securityContext = $this->container->get('security.context');
+        $user = $securityContext->getToken()->getUser();
+        $auth_token=$user->getAuthToken();
+        if($auth_token){
+            return $this->redirect($this->generateUrl('device_browser_image_edit', array('auth_token'=>$auth_token)));
+        }else{
+            return new Response('Authentication error');
         }
+        
     }
 }
 
