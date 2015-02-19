df174bd24080e44875cd3000facedc1d72b0f7df
Waqas Muddasir <waqas.muddasir@centricsource.com>
2015-02-19 12:36:21 +0500

    Algorithm 2 started

--- src/LoveThatFit/AdminBundle/Controller/AlgorithmController.php
+++ src/LoveThatFit/AdminBundle/Controller/AlgorithmController.php
@@ -9,6 +9,7 @@ use LoveThatFit\AdminBundle\Form\Type\AlgoritumTestlType;
 use LoveThatFit\AdminBundle\Form\Type\AlgoritumProductTestlType;
 use LoveThatFit\SiteBundle\Comparison;
 use LoveThatFit\SiteBundle\AvgAlgorithm;
+use LoveThatFit\SiteBundle\DependencyInjection\FitAlgorithm2;
 
 class AlgorithmController extends Controller {
 
@@ -39,9 +40,37 @@ class AlgorithmController extends Controller {
     }
 
     //------------------------------------------------------------------------------------------
-    ################################################################
-    ################################################################
+################################################################
+#   Fit Algorithm 2    
+################################################################
 
+    public function fitAlgorithmIndexAction() {
+        $userForm = $this->createForm(new AlgoritumTestlType());
+        $productForm = $this->createForm(new AlgoritumProductTestlType());
+        return $this->render('LoveThatFitAdminBundle:Algoritm:avg_index.html.twig', array(
+                    'userForm' => $userForm->createView(),
+                    'productForm' => $productForm->createView(),
+                    'user' => '',
+                ));
+    }
+//------------------------------------------------------------------------------------------
+    
+    public function fitAlgorithmCompareAction($user_id, $product_id, $json) {
+        $product = $this->get('admin.helper.product')->find($product_id);
+        $user = $this->get('user.helper.user')->find($user_id);
+        $fe = new AvgAlgorithm($user, $product);
+        #return new Response(json_encode($fe->getComparison()));  
+        #return new Response(json_encode($fe->getRecommendation()));        
+        $fb = $fe->getFeedback();
+        if ($json) {
+            return new Response(json_encode($fb));
+        } else {
+            return $this->render('LoveThatFitAdminBundle:Algoritm:_avg_comparison.html.twig', array(
+                        'product' => $product, 'user' => $user, 'data' => $fb,
+                    ));
+        }
+    }
+############################################################################
     public function compareAction($user_id, $product_id, $json) {
         $product = $this->get('admin.helper.product')->find($product_id);
         $user = $this->get('user.helper.user')->find($user_id);
@@ -75,18 +104,12 @@ class AlgorithmController extends Controller {
     #defaults: { _controller: LoveThatFitAdminBundle:Algorithm:foo, user_id: 0, product_id: 0, size_id: 0}
 
 
-    public function fooAction($user_id, $product_id, $size_id=0) {
+    public function fooAction($user_id=0, $product_id=0, $size_id=0) {
+        $user_id=66; $product_id=5;
         $product = $this->get('admin.helper.product')->find($product_id);
         $user = $this->get('user.helper.user')->find($user_id);
-        #~~~~~~~~~~~~~ comparision strip
-        //$comp = new Comparison($user, $product);        
-        //return new Response($comp->getStrippedFeedBack());
-        #~~~~~~~~~~~~~ avg strip
-        $comp = new AvgAlgorithm($user, $product);                
-        return new Response($comp->getStrippedFeedBackJSON());
-        
-        //return new Response(json_encode($product->getDetailArray()+$fe->getFeedBack()));
-        
+        $fa = new FitAlgorithm2($user, $product);                
+        return new response($fa->getFeedBackJSON());
     }
 
 }
