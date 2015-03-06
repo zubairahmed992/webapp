4a9329b7ae82ca0bc3f62e583e1d2f9ba4b3857a
m-ibrahim-masudi-cs <muhammad.ibrahim@centricsource.com>
2015-03-06 19:52:58 +0500

    SVG path for iOS camera app, in between
    SVG path for iOS camera app, in between

---
+++ web/bundles/lovethatfit/site/js/default_mask_marker.js
@@ -0,0 +1,210 @@
+hitOptions = {
+	segments: true,
+	stroke: false,
+	fill: false,
+	tolerance: 22
+};
+
+curr_screen_height = 505  ;
+center_pos = 161;
+
+inc_ratio = 1;
+
+dv_user_px_h = parseInt($("#dv_user_px_height").attr("value"));
+dv_top_bar = parseInt($("#dv_top_bar").attr("value"));
+dv_bottom_bar = parseInt($("#dv_bottom_bar").attr("value"));
+dv_per_inch_px = parseInt($("#dv_per_inch_px").attr("value"));
+dv_type = parseInt($("#dv_type").attr("value"));
+dv_scr_h = parseInt($("#dv_scr_h").attr("value"));
+dv_edit_type = parseInt($("#dv_edit_type").attr("value"));
+
+fixed_px_inch_ratio = 6.10;
+
+//////// From JS file
+
+chk_no_img_path = true;
+
+$(document).ready(function() {
+    createBlob();
+});
+
+function createBlob() {
+ 
+path_data = $("#default_user_path").html();
+ 
+mid_area_path = new Path(path_data);
+mid_area_path.opacity = 0.6;
+
+
+var p_user_height = parseInt($('#user_height_frm_3').attr('value')) + 3.375;
+
+p_user_height = p_user_height * fixed_px_inch_ratio;
+
+p_user_height = p_user_height * 100 / 450;
+
+p_user_height = p_user_height / 100;
+
+console.log(p_user_height);
+
+
+if(chk_no_img_path == true){
+            mid_area_path.pivot = new Point(0,435);
+            mid_area_path.scale(inc_ratio, p_user_height);
+           
+   ///////////Check Impect///////////
+        if(parseInt($('#user_height_frm_3').attr('value')) >= 75){
+            def_head_p_incr = (parseInt($('#user_height_frm_3').attr('value')) - 75)/5;
+        }
+        else{
+            def_head_p_incr = (75 - parseInt($('#user_height_frm_3').attr('value')))/6;
+        }
+
+  var head_segments = [1,2,3,4,5,6,7,69,68,67,66,65,64,63];
+  function adj_head_points(){
+      for(var i = 0; i < head_segments.length; i++) {
+          if(head_segments[i] == 5 || head_segments[i] == 65) {
+            mid_area_path.segments[head_segments[i]].point.y += def_head_p_incr * 1.5;
+          }
+          
+          else {
+            mid_area_path.segments[head_segments[i]].point.y +=  def_head_p_incr;
+          }
+      };
+  }
+  adj_head_points();
+  
+  var torso_adj_segments = [16,17,18,19,20,21,35,54,53,52,51,50,49];
+  function adj_torso_points(){
+        
+  //var arm_pit_dis = mid_area_path.segments[54].point.y - mid_area_path.segments[64].point.y;
+  var arm_pit_dis = 43;
+  var arm_pit_dis_curr = mid_area_path.segments[54].point.y - mid_area_path.segments[64].point.y;
+  var final_arm_pit_dis = (arm_pit_dis - arm_pit_dis_curr) + def_head_p_incr;
+  
+      for(var i = 0; i < torso_adj_segments.length; i++) {
+          if(false) {
+            mid_area_path.segments[torso_adj_segments[i]].point.y = (mid_area_path.segments[64].point.y + arm_pit_dis);
+          }
+          else {
+            mid_area_path.segments[torso_adj_segments[i]].point.y += final_arm_pit_dis;
+          }
+      };
+  }
+  adj_torso_points();
+  
+  
+  var inseam_adj_segments = [22,23,24,25,48,47,46,45,44];
+  function adj_inseam_points(){
+        
+  var arm_pit_dis = 43;
+  var arm_pit_dis_curr = mid_area_path.segments[54].point.y - mid_area_path.segments[64].point.y;
+  var final_arm_pit_dis = (arm_pit_dis - arm_pit_dis_curr) + def_head_p_incr;
+  
+      for(var i = 0; i < inseam_adj_segments.length; i++) {
+          if(false) {
+            mid_area_path.segments[inseam_adj_segments[i]].point.y = (mid_area_path.segments[64].point.y + arm_pit_dis);
+          }
+          else {
+            mid_area_path.segments[inseam_adj_segments[i]].point.y += final_arm_pit_dis;
+          }
+      };
+  }
+  adj_inseam_points();
+  
+  
+  var user_shoulder_width = parseInt($("#user_back_frm_3").attr("value"));
+  
+  user_shoulder_width = user_shoulder_width * fixed_px_inch_ratio;
+  
+  //alert(dv_per_inch_px);
+  
+  var torso_w_adj_left = [54,53,52,51,50,49];
+  var torso_w_adj_right = [16,17,18,19,20,21];
+  
+  var dm_front_shoulder = mid_area_path.segments[7].point.x - mid_area_path.segments[63].point.x;
+  var user_front_shoulder = user_shoulder_width;
+  
+  var front_shoulder_diff = user_front_shoulder - dm_front_shoulder;
+  //alert(front_shoulder_diff/2);
+  mid_area_path.segments[7].point.x += front_shoulder_diff/2;
+  mid_area_path.segments[63].point.x -= front_shoulder_diff/2;
+  
+  //var front_shoulder_diff = 60;
+  
+  var diff_apply = front_shoulder_diff/2;
+
+  function adj_torso_points_w(){
+    for(var i = 0; i < torso_w_adj_left.length; i++) {
+          mid_area_path.segments[torso_w_adj_left[i]].point.x -= diff_apply;
+    };
+    for(var i = 0; i < torso_w_adj_right.length; i++) {
+          mid_area_path.segments[torso_w_adj_right[i]].point.x += diff_apply;
+    };
+  }
+  adj_torso_points_w();
+  
+  
+  var arm_w_adj_out_left = [8,9,10];
+  var arm_w_adj_out_right = [62,61,60];
+  var diff_apply = front_shoulder_diff/4;
+  
+  mid_area_path.segments[8].point.x += (diff_apply*2)+((diff_apply*32)/100);
+  mid_area_path.segments[9].point.x += (diff_apply*2)+((diff_apply*32)/100);
+  mid_area_path.segments[10].point.x += (diff_apply*2)+((diff_apply*32)/100);
+  mid_area_path.segments[62].point.x -= (diff_apply*2)-((diff_apply*32)/100);
+  mid_area_path.segments[61].point.x -= (diff_apply*2)-((diff_apply*32)/100);
+  mid_area_path.segments[60].point.x -= (diff_apply*2)-((diff_apply*32)/100);
+  
+  mid_area_path.segments[14].point.x += diff_apply*2;
+  mid_area_path.segments[15].point.x += diff_apply*2;
+  
+  mid_area_path.segments[14].point.x -= ((diff_apply*32)/100);
+  mid_area_path.segments[15].point.x -= ((diff_apply*32)/100);
+  
+  mid_area_path.segments[56].point.x -= diff_apply*2;
+  mid_area_path.segments[55].point.x -= diff_apply*2;
+  
+  mid_area_path.segments[56].point.x += (diff_apply*32)/100;
+  mid_area_path.segments[55].point.x += (diff_apply*32)/100;
+  
+  
+  mid_area_path.segments[48].point.x -= front_shoulder_diff*54/100;
+  
+  mid_area_path.segments[47].point.x -= front_shoulder_diff*44/100;
+  mid_area_path.segments[46].point.x -= front_shoulder_diff*44/100;
+  mid_area_path.segments[45].point.x -= front_shoulder_diff*44/100;
+  
+  mid_area_path.segments[44].point.x -= ((front_shoulder_diff*33.5)/100);
+  
+  mid_area_path.segments[43].point.x -= ((diff_apply*33.5)/100);
+
+  mid_area_path.segments[22].point.x += front_shoulder_diff*54/100;
+  mid_area_path.segments[23].point.x += ((front_shoulder_diff*44)/100);
+  mid_area_path.segments[24].point.x += ((front_shoulder_diff*44)/100);
+  mid_area_path.segments[25].point.x += ((front_shoulder_diff*44)/100);
+  
+  mid_area_path.segments[26].point.x += ((front_shoulder_diff*44)/100);
+  
+  mid_area_path.segments[27].point.x += ((diff_apply*33.5)/100);
+  
+  
+ 
+    mid_area_path.segments[34].point.x -= ((front_shoulder_diff*14)/100);
+    mid_area_path.segments[36].point.x += ((front_shoulder_diff*14)/100);
+    
+    mid_area_path.segments[33].point.x -= ((front_shoulder_diff*11)/100);
+    mid_area_path.segments[37].point.x += ((front_shoulder_diff*11)/100);
+    
+    mid_area_path.segments[31].point.x -= ((front_shoulder_diff*7.5)/100);
+    mid_area_path.segments[39].point.x += ((front_shoulder_diff*7.5)/100);
+    
+}
+    mid_area_path.selected = true;
+    mid_area_path.strokeWidth = 1;
+    mid_area_path.strokeColor = new Color(1, 0, 0);
+    mid_area_path.opacity = 0.85;
+
+    $("#svg_path_data").attr("value", mid_area_path.pathData);
+    window.location.href = "svg_path_created";
+    return mid_area_path;
+}
