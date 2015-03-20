8080651b41f93a5ce6a31136e1cce4760566719f
waqas muddasir <swm@winCS>
2015-03-20 23:23:46 +0500

    user image resize method added

--- src/LoveThatFit/UserBundle/Entity/User.php
+++ src/LoveThatFit/UserBundle/Entity/User.php
@@ -1716,4 +1716,47 @@ class User implements UserInterface, \Serializable {
         return $obj;
         
     }
+    
+    #-----------------------------------------------------------
+     public function resize_image() {
+
+        $filename = $this->getAbsolutePath();
+        $image_info = @getimagesize($filename);
+        $image_type = $image_info[2];
+
+        switch ($image_type) {
+            case IMAGETYPE_JPEG:
+                $source = imagecreatefromjpeg($filename);
+                break;
+            case IMAGETYPE_GIF:
+                $source = imagecreatefromgif($filename);
+                break;
+            case IMAGETYPE_PNG:
+                $source = imagecreatefrompng($filename);
+                break;
+        }
+        #------------ Need dimensions
+        $width=50;
+        $height=50;
+        $img_new = imagecreatetruecolor($width, $height);
+        imagealphablending($img_new, false);
+        imagesavealpha($img_new,true);
+        $transparent = imagecolorallocatealpha($img_new, 255, 255, 255, 127);
+        imagefilledrectangle($img_new, 0, 0, $width, $height, $transparent);
+        imagecopyresampled($img_new, $source, 0, 0, 0, 0, $width, $height, imagesx($source), imagesy($source));
+
+        switch ($image_type) {
+            case IMAGETYPE_JPEG:
+                imagejpeg($img_new, $filename, 75);
+                break;
+            case IMAGETYPE_GIF:
+                imagegif($img_new, $filename);
+                break;
+            case IMAGETYPE_PNG:
+                imagepng($img_new, $filename);
+                break;
+        }
+      
+        
+    }
 }
