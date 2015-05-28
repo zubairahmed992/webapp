06ccf9d83c88c53373f88d2d6088f4e98f5fbbab
Waqas Muddasir <waqas.muddasir@centricsource.com>
2015-05-28 12:48:49 +0500

    service helper cleared out

--- src/LoveThatFit/WebServiceBundle/Controller/ServiceInterfaceController.php
+++ src/LoveThatFit/WebServiceBundle/Controller/ServiceInterfaceController.php
@@ -5,20 +5,19 @@ namespace LoveThatFit\WebServiceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Yaml\Parser;
 
 use LoveThatFit\WebServiceBundle\Form\Type\ServiceFormType;
 class ServiceInterfaceController extends Controller {
 
     
     public function indexAction() {
-        $conf = $this->get('webservice.helper')->getServiceDetails();
-        $names = $this->get('webservice.helper')->stripToNameArray($conf);
+        $conf = $this->getServiceDetails();
+        $names = $this->stripToNameArray($conf);
         $form = $this->createForm(new ServiceFormType($names), array('message' => 'web service type'));
         $user_list= $this->get('user.helper.user')->getListWithPagination(0,'email');
         
-        #$str=json_encode($this->get('webservice.helper')->getServiceNames());
-        #return new Response(json_encode($conf));
-         return $this->render('LoveThatFitWebServiceBundle:ServiceInterface:index.html.twig', array(
+        return $this->render('LoveThatFitWebServiceBundle:ServiceInterface:index.html.twig', array(
                     'form' => $form->createView(),
                     'services_array'=>$conf,
                     'services_json'=>json_encode($conf),
@@ -27,7 +26,7 @@ class ServiceInterfaceController extends Controller {
     }
     #------------------------------------------------------
     public function hitAction() {    
-        $str=json_encode($this->get('webservice.helper')->stripToNameArray($this->get('webservice.helper')->getServiceDetails()));
+        $str=json_encode($this->stripToNameArray($this->getServiceDetails()));
         return new Response($str);       
     }
    #------------------------------------------------------
@@ -44,5 +43,34 @@ class ServiceInterfaceController extends Controller {
     public function barAction() {
         return new Response('oohoeyy');
     }
+    
+    
+    #------------------------------------------------------------------------
+    private function getServiceNames(){
+        $yaml = new Parser();
+        $conf = $yaml->parse(file_get_contents('../src/LoveThatFit/WebServiceBundle/Resources/config/routing.yml'));
+        $ar=array();
+        foreach($conf as $k=>$v){            
+            if (preg_match("!ws_(.*)!", $k)) {                
+                array_push($ar, str_replace("ws_","",$k));    
+            }            
+        }
+        return $ar;
+    }
+#------------------------------------------------------------------------
+    private function stripToNameArray($conf){
+        $ar=array();
+        foreach($conf as $k=>$v){            
+                array_push($ar, $k);                
+        }
+        return $ar;
+    }
+#------------------------------------------------------------------------    
+    private function getServiceDetails(){
+        $yaml = new Parser();
+        $conf = $yaml->parse(file_get_contents('../src/LoveThatFit/WebServiceBundle/Resources/config/services_list.yml'));        
+        return $conf;
+    }
+
     
 }
--- src/LoveThatFit/WebServiceBundle/Entity/WebServiceHelper.php
+++ src/LoveThatFit/WebServiceBundle/Entity/WebServiceHelper.php
@@ -1,34 +1,16 @@
 <?php
 
 namespace LoveThatFit\WebServiceBundle\Entity;
-use Symfony\Component\Yaml\Parser;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
 
 
 class WebServiceHelper {
 
-    public function getServiceNames(){
-        $yaml = new Parser();
-        $conf = $yaml->parse(file_get_contents('../src/LoveThatFit/WebServiceBundle/Resources/config/routing.yml'));
-        $ar=array();
-        foreach($conf as $k=>$v){            
-            if (preg_match("!ws_(.*)!", $k)) {                
-                array_push($ar, str_replace("ws_","",$k));    
-            }            
-        }
-        return $ar;
-    }
+     private $container;
 
-    public function stripToNameArray($conf){
-        $ar=array();
-        foreach($conf as $k=>$v){            
-                array_push($ar, $k);                
-        }
-        return $ar;
+    public function __construct(Container $container) {
+      $this->container = $container;
     }
-    public function getServiceDetails(){
-        $yaml = new Parser();
-        $conf = $yaml->parse(file_get_contents('../src/LoveThatFit/WebServiceBundle/Resources/config/services_list.yml'));        
-        return $conf;
-    }
+    
     
 }
