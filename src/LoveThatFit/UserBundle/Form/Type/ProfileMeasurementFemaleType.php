74d070b7d09095db07b6a9bfa72ad0ef86634112
waqas muddasir <swm@winCS>
2015-06-16 17:59:19 +0500

    shoulder height removed from profile

--- src/LoveThatFit/UserBundle/Form/Type/ProfileMeasurementFemaleType.php
+++ src/LoveThatFit/UserBundle/Form/Type/ProfileMeasurementFemaleType.php
@@ -19,7 +19,7 @@ class ProfileMeasurementFemaleType extends AbstractType
         $builder->add('hip', 'number', array('precision' => 2,'required'=>false));        
         $builder->add('inseam', 'number', array('precision' => 2,'required'=>false));        
         $builder->add('outseam', 'number', array('precision' => 2,'required'=>false));
-        $builder->add('shoulder_height', 'number', array('precision' => 2,'required'=>false));
+        
         $builder->add('weight', 'number', array('precision' => 2,'required'=>false));       
         $builder->add('thigh', 'number', array('precision' => 2,'required'=>false));
         $builder->add('shoulder_across_front', 'number', array('precision' => 2,'required'=>false));
