24d038f36622d023038565aee70f330784ebde9a
m-ibrahim-masudi-cs <muhammad.ibrahim@centricsource.com>
2015-07-09 19:11:04 +0500

    Visual Comparison: In between

--- web/bundles/lovethatfit/site/js/visual_comparison.js
+++ web/bundles/lovethatfit/site/js/visual_comparison.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
-    load_json();
+    load_user_masks();
 });
 
-function load_json(){
+function load_user_masks(){
     var json_data = jQuery.parseJSON($("#json_data_static").html());
-    mid_area_path = new Path(pathData);
-    alert(json_data);
+    
+    for(var i = 0; i < json_data.length; i++) {
+      
+      if(json_data[i].email == "050315.buf.056@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com"){
+        user_mask = new Path(json_data[i].svg_paths);
+        user_mask.style = {
+	stroke: 1,
+	strokeColor: '#ffcc00'
+        }
+    }
+    };
+    //mid_area_path = new Path(pathData);
+    //alert(json_data);
 }
 
 main_layer = new Layer();
