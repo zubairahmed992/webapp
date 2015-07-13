8c9ae63de383426bf81e9c732a9ad6c5a46b2a3e
m-ibrahim-masudi-cs <muhammad.ibrahim@centricsource.com>
2015-07-13 11:57:24 +0500

    Visual Comparison Interface in between

--- web/bundles/lovethatfit/site/js/visual_comparison.js
+++ web/bundles/lovethatfit/site/js/visual_comparison.js
@@ -4,15 +4,22 @@ $(document).ready(function() {
 
 function load_user_masks(){
     var json_data = jQuery.parseJSON($("#json_data_static").html());
-    
+    var p_extra_foot_area = 22;
     for(var i = 0; i < json_data.length; i++) {
       
       if(json_data[i].email == "050315.buf.056@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com"){
         user_mask = new Path(json_data[i].svg_paths);
+        user_mask.pivot = new Point(user_mask.bounds.bottomCenter.x,user_mask.bounds.bottomCenter.y - p_extra_foot_area); /// Setting pivot point before x, y setting, as it is done in registration///
+        user_mask.position = new Point(json_data[i].mask_x, json_data[i].mask_y);
         user_mask.style = {
 	stroke: 1,
-	strokeColor: '#ffcc00'
+	strokeColor: {
+                hue: Math.random() * 360,
+                saturation: 1,
+                brightness: 1
+            }
         }
+        //user_mask.selected = true;
     }
     };
     //mid_area_path = new Path(pathData);
