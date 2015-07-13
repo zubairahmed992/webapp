eb1898835192b6d3a5abae495bd1e8a992485539
m-ibrahim-masudi-cs <muhammad.ibrahim@centricsource.com>
2015-07-13 20:47:34 +0500

    Visual Interface: In between

--- web/bundles/lovethatfit/site/js/visual_comparison.js
+++ web/bundles/lovethatfit/site/js/visual_comparison.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-    load_user_masks();
+    alert("sd");
+    //load_user_masks();
 });
 
 function load_user_masks(){
-    var json_data = jQuery.parseJSON($("#json_data_static").html());
-    var p_extra_foot_area = 22;
+    var json_data = jQuery.parseJSON($("#user_data_placeholder").html());
+    
+    
+    
     for(var i = 0; i < json_data.length; i++) {
       
-      if(json_data[i].email == "050315.buf.056@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com"){
+      //if(json_data[i].email == "050315.buf.056@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com"){
         user_mask = new Path(json_data[i].svg_paths);
         
         ////---- Align mask markers from bottom, same as in iPhone5////
+        ////var p_extra_foot_area = 22; //For foot height adjustment
         ////user_mask.pivot = new Point(user_mask.bounds.bottomCenter.x,user_mask.bounds.bottomCenter.y - p_extra_foot_area); /// Setting pivot point before x, y setting, as it is done in registration///
         ////user_mask.position = new Point(json_data[i].mask_x, json_data[i].mask_y);
         ////----------------------------------/////
@@ -29,7 +33,9 @@ function load_user_masks(){
             }
         }
         //user_mask.selected = true;
-    }
+    
+    //////------commenting if closing bras }
+    
     };
     //mid_area_path = new Path(pathData);
     //alert(json_data);
