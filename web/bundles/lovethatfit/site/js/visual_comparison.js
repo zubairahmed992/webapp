72d901ccc1437cd1054cda16b6543dd05e0ca3c8
m-ibrahim-masudi-cs <muhammad.ibrahim@centricsource.com>
2015-07-14 12:38:21 +0500

    Visual Comparison Interface: In between

--- web/bundles/lovethatfit/site/js/visual_comparison.js
+++ web/bundles/lovethatfit/site/js/visual_comparison.js
@@ -1,16 +1,15 @@
+paper.install(window);
+
 $(document).ready(function() {
-    alert("sd");
-    //load_user_masks();
+    load_user_masks();
 });
 
 function load_user_masks(){
-    var json_data = jQuery.parseJSON($("#user_data_placeholder").html());
-    
-    
-    
+    paper.setup('canv_compare');
+    var json_data = jQuery.parseJSON($("#user_data_placeholder").html());    
     for(var i = 0; i < json_data.length; i++) {
-      
-      //if(json_data[i].email == "050315.buf.056@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com"){
+        
+        //if(json_data[i].email == "050315.buf.056@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com" || json_data[i].email == "050315.buf.103@ltf.com"){
         user_mask = new Path(json_data[i].svg_paths);
         
         ////---- Align mask markers from bottom, same as in iPhone5////
@@ -23,29 +22,32 @@ function load_user_masks(){
         user_mask.pivot = new Point(user_mask.bounds.topCenter.x,user_mask.bounds.topCenter.y);
         user_mask.position = new Point(320/2, 0);
         
+        var r_value = Math.round(Math.random() * 250/100);
+        var g_value = Math.round(Math.random() * 250/100);
+        var b_value = Math.round(Math.random() * 250/100);
         
+        var masks_m_down = new Tool();
+        masks_m_down.onMouseDown = onMouseDown;
+        
+        $("#mask_ids").append('<div class="mask_ref_id">'+json_data[i].id+'<div class="mask_spot" style="background: rgb(' + r_value * 100 + ',' + g_value * 100 + ',' + b_value * 100 + ')"></div></div>');
         user_mask.style = {
 	stroke: 1,
-	strokeColor: {
-                hue: Math.random() * 360,
-                saturation: 1,
-                brightness: 1
-            }
-        }
-        //user_mask.selected = true;
-    
+	strokeColor: new Color(r_value,g_value,b_value)
+        }    
     //////------commenting if closing bras }
+    //user_mask.strokeColor.hue += hue_value;
+    //user_mask.strokeColor.saturation = 0.5;
+    //user_mask.strokeColor.brightness = 0;
+    //user_mask.strokeColor.lightness 
     
+    
+    function onMouseDown(event) {
+        console.log("QQQQQQQQQQQQ");
+    }
+    
+    
+    view.update([true]);
     };
-    //mid_area_path = new Path(pathData);
-    //alert(json_data);
 }
-
-
-//var but_getdata_url = curr_path_prefix + "bundles/lovethatfit/site/images/back_button_app.png";
-//var but_back_top = new Raster(but_back_top_url);
-
-//but_back_top.position = new Point(25, 22);
-
-main_layer = new Layer();
-main_layer.activate();
+//main_layer = new Layer();
+//main_layer.activate();
