3932bcf240bf2c6853ff49b58bb0e174af4b53d2
m-ibrahim-masudi-cs <muhammad.ibrahim@centricsource.com>
2015-09-04 19:02:37 +0500

    Removed 1% adjustment & further adjustment of Mask
    Removed 1% adjustment & further adjustment of Mask Marker.

--- web/bundles/lovethatfit/site/js/default_mask_marker.js
+++ web/bundles/lovethatfit/site/js/default_mask_marker.js
@@ -28,16 +28,17 @@ if(liquid_mask.device_type == "iphone5"){
     fixed_px_inch_ratio = 6.891;
     
     // adjusting 66.666% value of top empty area ----- 19.5/3*2 = 13
-    adj_btm_fix = 13;
+    // 3.83 is 1% value
+    adj_btm_fix = 13 + 3.83;
 }
 if(liquid_mask.device_type == "iphone6"){
     fixed_px_inch_ratio = 8.094;
     
     // adjusting 66.666% value of top empty area ----- 23/3*2 = 15.333
-    adj_btm_fix = 15.333;
+    // 4.49 is 1% value
+    adj_btm_fix = 15.333 + 4.49;
 }
 //////// From JS file
-
 chk_no_img_path = true;
 
 $(document).ready(function() {
@@ -253,7 +254,8 @@ if(chk_no_img_path == true){
     
     if(liquid_mask.device_type == "iphone5"){
       mid_area_path.scale(0.750, 0.750);
-      mid_area_path.scale(1, 1.01);
+      //One percent adjustment
+      //mid_area_path.scale(1, 1.01);
       mid_area_path.pivot = new Point(mid_area_path.bounds.bottomCenter.x,mid_area_path.bounds.bottomCenter.y);
       mid_area_path.position = new Point(160,403.50 - adj_btm_fix);
       
@@ -267,9 +269,9 @@ if(chk_no_img_path == true){
     }
     if (liquid_mask.device_type == "iphone6"){
       //mid_area_path.scale(0.9, 0.9);
-      //mid_area_path.scale(1, 1.01);
       mid_area_path.scale(0.748, 0.748);
-      mid_area_path.scale(1, 1.01);
+      //One percent adjustment
+      //mid_area_path.scale(1, 1.01);
       mid_area_path.pivot = new Point(mid_area_path.bounds.bottomCenter.x,mid_area_path.bounds.bottomCenter.y);
       mid_area_path.position = new Point(screen.width/2,472 + 2 - adj_btm_fix);
       //alert("6_6_6");
