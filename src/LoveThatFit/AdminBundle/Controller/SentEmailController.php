84d2a590eb962364d329403a7181d6cf7f46ad28
waqas muddasir <swm@winCS>
2015-10-27 23:26:04 +0500

    sent email viewer WIP

---
+++ src/LoveThatFit/AdminBundle/Controller/SentEmailController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LoveThatFit\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use LoveThatFit\UserBundle\Entity\User;
+
+class SentEmailController extends Controller {
+
+    public function showAction($email_type, $id) {
+        $userParentChilds=$this->get('user.helper.parent.child')->find($id);
+        return new Response($userParentChilds->getEmail());
+    }
+
+}
