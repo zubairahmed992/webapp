d0962b09e7bd2c031086761d6f64479462ef5293
waqas muddasir <swm@winCS>
2015-10-28 23:19:33 +0500

    email visual WIP

--- src/LoveThatFit/AdminBundle/Controller/SentEmailController.php
+++ src/LoveThatFit/AdminBundle/Controller/SentEmailController.php
@@ -11,7 +11,8 @@ class SentEmailController extends Controller {
 
     public function showAction($email_type, $id) {
         $userParentChilds=$this->get('user.helper.parent.child')->find($id);
-        return new Response($userParentChilds->getEmail());
+        #return new Response($userParentChilds->getEmail());
+        return $this->render('LoveThatFitAdminBundle::email/parent_registration.html.twig', array('entity' => $userParentChilds, 'reset_link' => ''));
     }
 
 }
