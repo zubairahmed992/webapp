688a8eb34110355c097636f548cf61ebde3482a3
waqas muddasir <swm@winCS>
2015-12-04 23:14:55 +0500

    visitors entity added, layout & content changes

---
+++ src/LoveThatFit/SiteBundle/Entity/Visitor.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace LoveThatFit\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * LoveThatFit\SiteBundle\Entity\Visitor
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="LoveThatFit\SiteBundle\Entity\Visitor")
+ */
+class Visitor
+{    
+      /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+     //----------------------------------------------------------
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+     public function __construct()
+    {
+    }
+  
+    /**
+     * @var \DateTime $created_at
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $created_at;
+
+  
+
+    /**
+     * @var string $email
+     *
+     * @ORM\Column(name="email", type="string",nullable=true)
+     */
+    private $email;
+    
+    /**
+     * @var string $ip_address
+     *
+     * @ORM\Column(name="ip_address", type="string",nullable=true)
+     */
+    private $ip_address;
+    
+    
+    /**
+     * @var string $browser
+     *
+     * @ORM\Column(name="browser", type="string",nullable=true)
+     */
+    private $browser;
+    
+    /**
+     * @var string $device
+     *
+     * @ORM\Column(name="device", type="string",nullable=true)
+     */
+    private $device;
+    
+    
+    /**
+     * @var string $country
+     *
+     * @ORM\Column(name="country", type="string",nullable=true)
+     */
+    private $country;
+    
+    
+  #----------------------------------------------------------------  
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return Visitor
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+#----------------------------------------------------------------  
+   
+    /**
+     * Set device
+     *
+     * @param string $device
+     * @return Visitor
+     */
+    public function setDevice($device)
+    {
+        $this->device = $device;
+    
+        return $this;
+    }
+
+    /**
+     * Get device
+     *
+     * @return string 
+     */
+    public function getDevice()
+    {
+        return $this->device;
+    }
+   #----------------------------------------------------------------  
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Visitor
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+   #----------------------------------------------------------------  
+   /**
+     * Set ip_address
+     *
+     * @param string $ip_address
+     * @return Visitor
+     */
+    public function setIpAddress($ip_address)
+    {
+        $this->ip_address = $ip_address;
+    
+        return $this;
+    }
+
+    /**
+     * Get $ip_address
+     *
+     * @return string 
+     */
+    public function getIpAddress()
+    {
+        return $this->ip_address;
+    }
+   #----------------------------------------------------------------  
+      /**
+     * Set browser
+     *
+     * @param string $browser
+     * @return Visitor
+     */
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
+    
+        return $this;
+    }
+
+    /**
+     * Get $browser
+     *
+     * @return string 
+     */
+    public function getBrowser()
+    {
+        return $this->browser;
+    }
+   #----------------------------------------------------------------  
+      /**
+     * Set country
+     *
+     * @param string $country
+     * @return Visitor
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    
+        return $this;
+    }
+
+    /**
+     * Get $country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+   #----------------------------------------------------------------  
+}
--- src/LoveThatFit/WebServiceBundle/Controller/VisitorController.php
+++ src/LoveThatFit/WebServiceBundle/Controller/VisitorController.php
@@ -2,16 +2,43 @@
 namespace LoveThatFit\WebServiceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use LoveThatFit\SiteBundle\Entity\Visitor;
 
 class VisitorController extends Controller {
 
 
     public function indexAction() {
+        return $this->render('LoveThatFitWebServiceBundle:Visitor:email.html.twig');
+        $v = new Visitor();
+        $v->setEmail('sfdsfdsfd');
+        $v->setBrowser($_SERVER['HTTP_USER_AGENT']);
+        $v->setDevice('sfdsfdsfd');
+        $v->setIpAddress('sfdsfdsfd');
+        $v->setCountry('sfdsfdsfd');
+        $v->setCreatedAt(new \DateTime('now'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($v);
+        $em->flush();
         #return new response(json_encode(get_browser(null, true)));
         return new response(json_encode($_SERVER['HTTP_USER_AGENT']));      
     }
     #---------------------------------------------------
-  public function saveVisitorInfoAction() {
+  public function saveInfoAction() {
+      
+        $decoded = $this->getRequest()->request->all();
+        return new response(var_dump($decoded["email"]));      
+      
+      
+        $v = new Visitor();
+        $v->setEmail($decoded['email']);
+        $v->setBrowser($_SERVER['HTTP_USER_AGENT']);
+        #$v->setDevice('sfdsfdsfd');
+        $v->setIpAddress($this->get_client_ip());
+        #$v->setCountry('sfdsfdsfd');
+        $v->setCreatedAt(new \DateTime('now'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($v);
+        $em->flush();
         return new response(json_encode('save visitor info'));      
     }    
     
