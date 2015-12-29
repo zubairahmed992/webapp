9a6dfc8ceeadf5a1499e99c40eb8f9f8b4d43635
waqas muddasir <swm@winCS>
2015-12-29 23:24:42 +0500

    mask marker admin edit interface

--- src/LoveThatFit/AdminBundle/Controller/MaskMarkerInspectController.php
+++ src/LoveThatFit/AdminBundle/Controller/MaskMarkerInspectController.php
@@ -5,7 +5,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Yaml\Parser;
-
+use LoveThatFit\UserBundle\Form\Type\MeasurementStepFourType;
+use LoveThatFit\UserBundle\Form\Type\RegistrationStepFourType;
+use LoveThatFit\UserBundle\Form\Type\MeasurementVerticalPositionFormType;
+use LoveThatFit\UserBundle\Form\Type\MeasurementHorizantalPositionFormType;
 
 class MaskMarkerInspectController extends Controller {
 
@@ -183,52 +186,64 @@ class MaskMarkerInspectController extends Controller {
                 ));
     }
 
-#############
-    public function _userDataDownloadAction() {
-        $users = $this->get('user.helper.user')->findAll();
-        header('Content-Type: application/csv');
-        header('Content-Disposition: attachement; filename="user_data.csv";');
-        // open the "output" stream
-        $f = fopen('php://output', 'w');
-         $headers= array(
-            'id',
-            'email', 
-            'name',
-            'zipcode',
-            'gender',
-            'birth_date',
-            'weight',
-            'height',
-            'waist',
-            'belt',
-            'hip',
-            'bust',
-            'chest',
-            'arm',
-            'inseam',
-            'shoulder_height',
-            'outseam',
-            'sleeve',
-            'neck',
-            'thigh',
-            'center_front_waist',
-            'shoulder_across_front',
-            'shoulder_across_back',
-            'bicep',
-            'tricep',
-            'wrist',
-            'back_waist', 
-            'waist_hip',
-            'knee',
-            'calf',
-            'ankle',
-        );
-         fputcsv($f, $headers);
-        foreach ($users as $u) {
-            //generate csv lines from the inner arrays
-            fputcsv($f, $u->toDataArray(false));
-        }
-        return new Response('');
+    ###################################################################################
+###################################################################################
+
+    public function maskImproveIndexAction(){
+          $form=$this->createForm(new \LoveThatFit\UserBundle\Form\Type\UserDropdownType());
+        return $this->render('LoveThatFitAdminBundle:MaskMarkerInspect:mask_improve_index.html.twig', array(
+                    'form' => $form->createView(),                    
+                ));
     }
+
+     #---------------------------------------------------------------------------
     
+   public function maskImproveEditAction($user_id) {
+       $user = $this->get('webservice.helper.user')->find($user_id);
+      # return  new Response ($user->getEmail());
+       $edit_type='registration';
+       $device_type='iphone5';
+       $device_type =$device_type==null? 'iphone5':$device_type;
+       if(!$user){
+           return new Response ('Authentication error');
+       }
+       
+        $measurement = $user->getMeasurement();               
+        if ($user->getUserMarker()->getDefaultUser()){# if demo account, then get measurement from json
+            $decoded=$measurement->getJSONMeasurement('actual_user');            
+            if(is_array($decoded)){
+                $measurement = $this->get('webservice.helper')->setUserMeasurementWithParams($decoded, $user);
+            }
+        }
+        $measurement_vertical_form = $this->createForm(new MeasurementVerticalPositionFormType(), $measurement);
+        $measurement_horizontal_form = $this->createForm(new MeasurementHorizantalPositionFormType(), $measurement);
+        $form = $this->createForm(new RegistrationStepFourType(), $user);
+        $measurement_form = $this->createForm(new MeasurementStepFourType(), $measurement);
+        $marker = $this->get('user.marker.helper')->getByUser($user);
+        $default_marker = $this->get('user.marker.helper')->getDefaultValuesBaseOnBodyType($user);
+       // return new response(json_encode($default_marker));
+        $device_spec = $user->getDeviceSpecs($device_type);
+        $device_screen_height = $this->get('admin.helper.utility')->getDeviceResolutionSpecs($device_type);
+        
+        $edit_type=$edit_type==null?'registration':$edit_type;
+        
+        return $this->render('LoveThatFitAdminBundle:MaskMarkerInspect:_mask_improve_show.html.twig', array(
+                    'form' => $form->createView(),               
+                    'measurement_form' => $measurement_form->createView(),                   
+                    'measurement_vertical_form' => $measurement_vertical_form->createView(),
+                    'measurement_horizontal_form' => $measurement_horizontal_form->createView(),
+                    'entity' => $user,
+                    'measurement' => $measurement,
+                    'edit_type' => $edit_type,
+                    'marker' => $marker,
+                    'default_marker' => $default_marker,
+                    'user_pixcel_height' =>  $device_spec->getUserPixcelHeight(),
+                    'top_bar' => $user->getMeasurement()->getIphoneHeadHeight(),
+                    'bottom_bar' => $user->getMeasurement()->getIphoneFootHeight(),
+                    'per_inch_pixcel' => $device_spec->getDeviceUserPerInchPixelHeight(),
+                    'device_type' => $device_type,
+                    'device_screen_height' => $device_screen_height['pixel_height'],
+            ));
+    
+}
 }
