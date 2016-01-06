de1a260c51dea7f87014a48e44ccca6fba951b79
Waqas Muddasir <waqas.muddasir@centricsource.com>
2016-01-07 02:07:24 +0500

    user marker adjustment changes

--- src/LoveThatFit/AdminBundle/Controller/UserMaskAdjustmentController.php
+++ src/LoveThatFit/AdminBundle/Controller/UserMaskAdjustmentController.php
@@ -24,9 +24,7 @@ class UserMaskAdjustmentController extends Controller {
    public function showCanvasAction($user_id) {
        $user = $this->get('webservice.helper.user')->find($user_id);
       # return  new Response ($user->getEmail());
-       $edit_type='edit';
-       $device_type='iphone5';
-       $device_type =$device_type==null? 'iphone5':$device_type;
+       $device_type =$user->getImageDeviceType()==null? 'iphone5':$user->getImageDeviceType();
        if(!$user){
            return new Response ('Authentication error');
        }
@@ -38,24 +36,17 @@ class UserMaskAdjustmentController extends Controller {
                 $measurement = $this->get('webservice.helper')->setUserMeasurementWithParams($decoded, $user);
             }
         }
-        $measurement_vertical_form = $this->createForm(new MeasurementVerticalPositionFormType(), $measurement);
-        $measurement_horizontal_form = $this->createForm(new MeasurementHorizantalPositionFormType(), $measurement);
         $form = $this->createForm(new RegistrationStepFourType(), $user);
-        $measurement_form = $this->createForm(new MeasurementStepFourType(), $measurement);
         $marker = $this->get('user.marker.helper')->getByUser($user);
         $default_marker = $this->get('user.marker.helper')->getDefaultValuesBaseOnBodyType($user);
-       // return new response(json_encode($default_marker));
         $device_spec = $user->getDeviceSpecs($device_type);
         $device_screen_height = $this->get('admin.helper.utility')->getDeviceResolutionSpecs($device_type);
         
-        $edit_type=$edit_type==null?'registration':$edit_type;
-        
         return $this->render('LoveThatFitAdminBundle:UserMaskAdjustment:_mask_canvas.html.twig', array(
                     'form' => $form->createView(),               
-                    'measurement_form' => $measurement_form->createView(),                   
-                       'entity' => $user,
+                    'entity' => $user,
                     'measurement' => $measurement,
-                    'edit_type' => $edit_type,
+                    'edit_type' => 'edit',
                     'marker' => $marker,
                     'default_marker' => $default_marker,
                     'user_pixcel_height' =>  $device_spec->getUserPixcelHeight(),
