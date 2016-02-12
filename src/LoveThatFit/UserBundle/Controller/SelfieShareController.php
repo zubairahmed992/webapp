be558d92572a0e107d22acf50ac3ec8625a2e35d
waqas muddasir <swm@winCS>
2016-02-12 23:25:01 +0500

    selfieshare feedback update changes

--- src/LoveThatFit/UserBundle/Controller/SelfieShareController.php
+++ src/LoveThatFit/UserBundle/Controller/SelfieShareController.php
@@ -18,9 +18,7 @@ class SelfieShareController extends Controller {
     #----------------------------------------------
 
     public function feedbackUpdateAction() {
-        $this->Request->request->all();
-
-        $ra=$this->process_request();
+        $ra=$this->getRequest()->request->all();
         #return new Response(json_encode($ra));
         $selfieshare=$this->get('user.selfieshare.helper')->updateFeedback($ra);            
         return new Response($selfieshare->getFriendName().'provided feedback updated.');
@@ -31,19 +29,6 @@ class SelfieShareController extends Controller {
     public function feedbackShowAction($ref=null) {
          $selfieshare=$this->get('user.selfieshare.helper')->findByRef($ref);            
          return $this->render('LoveThatFitUserBundle:Selfieshare:feedback_show.html.twig', array('selfieshare' => $selfieshare));   
-    }
-
-    #----------------------------------------------
-     private function process_request() {
-        $request=$this->getRequest();
-        $handle = fopen('php://input', 'r');
-        $jsonInput = fgets($handle);
-        $decoded = json_decode($jsonInput, true);
-
-        if ($decoded == null) #if null (to be used for web service testing))
-            $decoded = $request->request->all();
-
-        return $decoded;
     }
 
 }
