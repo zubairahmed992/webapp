8a8cbdb6e406c7283f56e6a2756538a36be0d65f
waqas muddasir <swm@winCS>
2016-07-22 22:39:12 +0500

    masked marker adjustment removed

--- src/LoveThatFit/AdminBundle/Controller/UserMaskAdjustmentController.php
+++ src/LoveThatFit/AdminBundle/Controller/UserMaskAdjustmentController.php
@@ -12,55 +12,7 @@ use LoveThatFit\UserBundle\Form\Type\MeasurementHorizantalPositionFormType;
 
 class UserMaskAdjustmentController extends Controller {
 
-      public function indexAction(){
-          $form=$this->createForm(new \LoveThatFit\UserBundle\Form\Type\UserDropdownType());
-        return $this->render('LoveThatFitAdminBundle:UserMaskAdjustment:index.html.twig', array(
-                    'form' => $form->createView(),                    
-                ));
-    }
-
-     #---------------------------------------------------------------------------
     
-   public function showCanvasAction($user_id) {
-       $user = $this->get('user.helper.user')->find($user_id);
-       
-      # return  new Response ($user->getEmail());
-       $device_type =$user->getImageDeviceType()==null? 'iphone5':$user->getImageDeviceType();
-       if(!$user){
-           return new Response ('Authentication error');
-       }
-       
-        $measurement = $user->getMeasurement();               
-        if ($user->getUserMarker()->getDefaultUser()){# if demo account, then get measurement from json
-            $decoded=$measurement->getJSONMeasurement('actual_user');            
-            if(is_array($decoded)){
-                $measurement = $this->get('webservice.helper')->setUserMeasurementWithParams($decoded, $user);
-            }
-        }
-        $form = $this->createForm(new RegistrationStepFourType(), $user);
-        $marker = $this->get('user.marker.helper')->getByUser($user);
-        $default_marker = $this->get('user.marker.helper')->getDefaultValuesBaseOnBodyType($user);
-        $device_spec = $user->getDeviceSpecs($device_type);
-        $device_screen_height = $this->get('admin.helper.utility')->getDeviceResolutionSpecs($device_type);
-        
-        return $this->render('LoveThatFitAdminBundle:UserMaskAdjustment:_mask_canvas.html.twig', array(
-                    'form' => $form->createView(),               
-                    'entity' => $user,
-                    'user_image_spec' => $user->getUserImageSpec(),
-                    'measurement' => $measurement,
-                    'edit_type' => 'edit',
-                    'marker' => $marker,
-                    'default_marker' => $default_marker,
-                    'user_pixcel_height' =>  $device_spec->getUserPixcelHeight(),
-                    'top_bar' => $user->getMeasurement()->getIphoneHeadHeight(),
-                    'bottom_bar' => $user->getMeasurement()->getIphoneFootHeight(),
-                    'per_inch_pixcel' => $device_spec->getDeviceUserPerInchPixelHeight(),
-                    'device_type' => $device_type,
-                    'device_screen_height' => $device_screen_height['pixel_height'],
-            ));
-
-}
-
 	//----------------All Pending User Display List --------------------------------------------------------------------------
   public function listAction($page_number, $sort = 'id') {
 	$orders_with_pagination = $this->get('user.helper.userarchives')->getListWithPagination($page_number, $sort);
