1f51f988a7f89a1dff79e6d693e4c6452bd6b385
behlool <behlool@centricsource.com>
2016-10-25 21:19:49 +0500

    Did work on registration steps

--- web/bundles/lovethatfit/site/js/apps.js
+++ web/bundles/lovethatfit/site/js/apps.js
@@ -1,52 +1,57 @@
 var app = angular.module('myApp', []);
 app.directive('validPasswordC', function() {
-  return {
-    require: 'ngModel',
-    scope: {
-      reference: '=validPasswordC'
-    },
-    link: function(scope, elm, attrs, ctrl) {
-      ctrl.$parsers.unshift(function(viewValue, $scope) {
-        var noMatch = viewValue != scope.reference
-        ctrl.$setValidity('noMatch', !noMatch);
-        return (noMatch)?noMatch:!noMatch;
-      });
+    return {
+        require: 'ngModel',
+        scope: {
+            reference: '=validPasswordC'
+        },
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue, $scope) {
+                var noMatch = viewValue != scope.reference
+                ctrl.$setValidity('noMatch', !noMatch);
+                return (noMatch)?noMatch:!noMatch;
+            });
 
-      scope.$watch("reference", function(value) {;
-        ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
-      });
+            scope.$watch("reference", function(value) {;
+                ctrl.$setValidity('noMatch', value === ctrl.$viewValue);
+            });
+        }
     }
-  }
 });
 app.controller('mainController', function($scope, $http) {
-    $scope.formData = {};
-    $scope.loginData = {};
+    $scope.regform = {};
     $scope.submitForm = function(){
-        var fdata = angular.toJson($scope.formData);
+        var fdata = angular.toJson($scope.regform);
         console.log(fdata);
         $http({
-          method  : 'POST',
-          url     : '../visitor/update',
-          data    : fdata, //forms user object
-          headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
+            method  : 'POST',
+            url     : '../visitor/update',
+            data    : fdata, //forms user object
+            headers : {'Content-Type': 'application/x-www-form-urlencoded'}
         }).success(function(data){
-              console.log(data);
-              alert('successfull save in db');
-         });
+            console.log(data);
+            // alert('successfull save in db');
+        });
     }
-    // {$scope.loginfrom = function(){
-    //     var ldata = angular.toJson($scope.loginData);
-    //      $http({
-    //      method  : 'POST',
-    //      url     : 'http://localhost/webapp/web/app_dev.php/login_check',
-    //      data    : ldata, //forms user object
-    //      headers : {'Content-Type': 'application/x-www-form-urlencoded'} 
-    //    }).success(function(data){
-     //         console.log(data);
-     //         alert('login save');
-     //    });
-     // }
 
 });// JavaScript Document
+
+app.controller('loginController', function($scope, $http) {
+    $scope.loginData = {};
+    $scope.loginfrom = function(){
+        var ldata = angular.toJson($scope.loginData);
+        $http({
+            method  : 'POST',
+            url     : '/webapp/web/app_dev.php/login_check',
+            data    : ldata, //forms user object
+            headers : {'Content-Type': 'application/x-www-form-urlencoded'}
+        }).success(function(data){
+            console.log(data);
+            //alert('login save');
+        });
+
+
+    }
+});
 
 
--- web/bundles/lovethatfit/site/js/formslider.js
+++ web/bundles/lovethatfit/site/js/formslider.js
@@ -1,117 +1,118 @@
 $(document).ready(function(){
     $("form").submit(function(e) {
-       // e.preventDefault();
+        // e.preventDefault();
     });
 
 
-  
-  var csplash = $("#content-splash");
-  var cregister = $("#content-register");
-  var cregister_2 = $("#content-register-step-2");
-  var clogin = $("#content-login");
- var greetings = $("#greetBox");
-  
+
+    var csplash = $("#content-splash");
+    var cregister = $("#content-register");
+    var cregister_2 = $("#content-register-step-2");
+    var clogin = $("#content-login");
+    var greetings = $("#greetBox");
+
     $("#register_btn").click(function(){
-         $("#regform").fadeOut('slow'); 
-         $("#greetBox").fadeIn().animate({
-      "left": "-880px"	  
-    }, 800, function(){ /* callback */ });
-        });  
-  
+        $("#regform").fadeOut('slow');
+        $("#greetBox").fadeIn().animate({
+            "left": "-880px"
+        }, 800, function(){ /* callback */ });
+    });
 
-   
-    
 
-  
-  /* display the register page */
-  $("#showsplash").on("click", function(e){ 	 
-   
-      e.preventDefault();   
-    $(cregister).css("display", "block");	
-    
-    $(csplash).stop().animate({
-	  "opacity": 0.25,
-      "left": "-880px"	  
-    }, 800, function(){ /* callback */ });
-  
-   
-    
-    
-	var winh = $(window).width();
-	if(winh >400){		
-      var shor = "207px";
-		}else{
-		 var shor = "-40px";
-		}	
-	
-	$(cregister).stop().animate({			
-      "left": shor		
-    }, 800, function(){ $(csplash).css("display", "none"); });
-    
-   
-  });
-  
-  
-  /* display the register page */
-  $("#showregister").on("click", function(e){	  
-    e.preventDefault();   
-    $(cregister_2).css("display", "block");	
-    
-    $(cregister).stop().animate({
-      "left": "-880px"
-    }, 800, function(){ /* callback */ });
-    
-    
-    
-	var winh = $(window).width();
-	if(winh >400){		
-      var shor = "207px";
-		}else{
-		 var shor = "-40px";
-		}	
-	$(cregister_2).stop().animate({
-      "left": shor
-    }, 800, function(){ $(cregister).css("display", "none"); });
-	
-	$(csplash).stop().animate({
-	  "opacity": 0.25,
-      "left": "-880px",	  
-    }, 800, function(){ /* callback */ });    
-    
-  });
-  
-  
 
-  /* display the login page */
-  $("#showlogin").on("click", function(e){
-    e.preventDefault();   
-    $(clogin).css("display", "block");
-	$('.loginbtn').hide();
-	
-	var winh = $(window).width();
-	if(winh >400){		
-      var shor = "207px";
-		}else{
-		 var shor = "-40px";
-		}	
-    
-    $(clogin).stop().animate({
-      "left": shor
-    }, 800, function() { /* callback */ });
-	
-	$(csplash).stop().animate({
-	  "opacity": 0.25,
-      "left": "-880px",	  
-    }, 800, function(){ $(csplash).css("display", "none"); });
-	
-    $(cregister).stop().animate({
-      "left": "880px"
-    }, 800, function() { $(cregister).css("display", "none"); });
-	
-	 $(cregister_2).stop().animate({
-      "left": "880px"
-    }, 800, function() { $(cregister_2).css("display", "none"); });	 
-    
-   
-  });
+
+
+
+    /* display the register page */
+    $("#showsplash").on("click", function(e){
+
+        e.preventDefault();
+        $(cregister).css("display", "block");
+
+        $(csplash).stop().animate({
+            "opacity": 0.25,
+            "left": "-880px"
+        }, 800, function(){ /* callback */ });
+
+
+
+
+        var winh = $(window).width();
+        if(winh >400){
+            var shor = "207px";
+        }else{
+            var shor = "-40px";
+        }
+
+        $(cregister).stop().animate({
+            "left": shor
+        }, 800, function(){ $(csplash).css("display", "none"); });
+
+
+    });
+
+
+    /* display the register page */
+    $("#showregister").on("click", function(e){
+        e.preventDefault();
+        $(cregister_2).css("display", "block");
+
+        $(cregister).stop().animate({
+            "left": "-880px"
+        }, 800, function(){ /* callback */ });
+
+
+
+        var winh = $(window).width();
+        if(winh >400){
+            var shor = "207px";
+        }else{
+            var shor = "-40px";
+        }
+        $(cregister_2).stop().animate({
+            "left": shor
+        }, 800, function(){ $(cregister).css("display", "none"); });
+
+        $(csplash).stop().animate({
+            "opacity": 0.25,
+            "left": "-880px",
+        }, 800, function(){ /* callback */ });
+
+    });
+
+
+
+    /* display the login page */
+    $("#showlogin").on("click", function(e){
+        e.preventDefault();
+        $(clogin).css("display", "block");
+        $('.loginbtn').hide();
+        $('#greetBox').hide();
+
+        var winh = $(window).width();
+        if(winh >400){
+            var shor = "207px";
+        }else{
+            var shor = "-40px";
+        }
+
+        $(clogin).stop().animate({
+            "left": shor
+        }, 800, function() { /* callback */ });
+
+        $(csplash).stop().animate({
+            "opacity": 0.25,
+            "left": "-880px",
+        }, 800, function(){ $(csplash).css("display", "none"); });
+
+        $(cregister).stop().animate({
+            "left": "880px"
+        }, 800, function() { $(cregister).css("display", "none"); });
+
+        $(cregister_2).stop().animate({
+            "left": "880px"
+        }, 800, function() { $(cregister_2).css("display", "none"); });
+
+
+    });
 });
