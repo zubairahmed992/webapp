bf2c8f8debefa0885552956929f7276647bfdf89
waqas muddasir <swm@winCS>
2016-12-06 23:30:59 +0500

    fit index show/edit

--- src/LoveThatFit/AdminBundle/Controller/FitModelSpecsController.php
+++ src/LoveThatFit/AdminBundle/Controller/FitModelSpecsController.php
@@ -7,9 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FitModelSpecsController extends Controller {
-
     #------------------------/admin/fit_model_specs/create_new
-    public function createNewAction(){        
+
+    public function createNewAction() {
         #return new Response(json_encode($this->get('admin.helper.product.specification')->getFitPoints()));
         $brands = $this->get('admin.helper.brand')->getBrnadArray();
         $clothing_types = $this->get('admin.helper.clothing_type')->getArray();
@@ -18,20 +18,20 @@ class FitModelSpecsController extends Controller {
         #return new Response(json_encode($size_specs));
         $fit_points = $this->get('admin.helper.product.specification')->getFitPoints();
         return $this->render('LoveThatFitAdminBundle:FitModelSpecs:create_new.html.twig', array(
-             'fit_points'=>$fit_points,
-             'brands'=>$brands,
-            'clothing_types' => $clothing_types,
-               'product_specs_json' => json_encode($product_specs),
-             'size_specs_json' => json_encode($size_specs),
-            
-         ));        
+                    'fit_points' => $fit_points,
+                    'brands' => $brands,
+                    'clothing_types' => $clothing_types,
+                    'product_specs_json' => json_encode($product_specs),
+                    'size_specs_json' => json_encode($size_specs),
+                ));
     }
+
     #------------------------/admin/fit_model_specs/save
-    public function saveAction(Request $request){        
+
+    public function saveAction(Request $request) {
         $decoded = $request->request->all();
-        
         $fmm = $this->get('admin.fit_model_measurement')->createNew();
-        $brand = $this->get('admin.helper.brand')->find($decoded['sel_brand']);        
+        $brand = $this->get('admin.helper.brand')->find($decoded['sel_brand']);
         $fmm->setBrand($brand);
         $fmm->setTitle($decoded['txt_title']);
         $fmm->setDescription($decoded['txt_title']);
@@ -39,31 +39,56 @@ class FitModelSpecsController extends Controller {
         $fmm->setClothingType($decoded['sel_clothing_type']);
         $fmm->setGender($decoded['sel_gender']);
         $fmm->setSizeTitleType($decoded['sel_size_type']);
-        $measurements=array();        
+        $measurements = array();
         $fit_points = $this->get('admin.helper.product.specification')->getFitPoints();
         foreach ($fit_points as $fp => $fp_title) {
-            if(array_key_exists($fp, $decoded)){
-            $measurements[$fp]=$decoded[$fp];
+            if (array_key_exists($fp, $decoded)) {
+                $measurements[$fp] = $decoded[$fp];
             }
         }
         $fmm->setMeasurementJson(json_encode($measurements));
         $this->get('admin.fit_model_measurement')->save($fmm);
         return new Response('saved!');
     }
+
     #------------------------/admin/fit_model_specs/index
-  public function indexAction(){ 
+
+    public function indexAction() {
         return $this->render('LoveThatFitAdminBundle:FitModelSpecs:index.html.twig', array(
-             'fit_model_measurements'=>$this->get('admin.fit_model_measurement')->findAll(),
-         ));        
+                    'fit_model_measurements' => $this->get('admin.fit_model_measurement')->findAll(),
+                ));
     }
+
     #------------------------/admin/fit_model_specs/show/id
-    public function showAction($id){ 
-        $fit_model_measurement=$this->get('admin.fit_model_measurement')->find($id);
-       $fit_points = json_decode($fit_model_measurement->getMeasurementJson(),true);
-       return $this->render('LoveThatFitAdminBundle:FitModelSpecs:show.html.twig', array(
-            'fit_model_measurement' => $fit_model_measurement,
-            'fit_points' => $fit_points,             
-         ));        
+
+    public function showAction($id) {
+        $fit_model_measurement = $this->get('admin.fit_model_measurement')->find($id);
+        $fit_points = json_decode($fit_model_measurement->getMeasurementJson(), true);
+        return $this->render('LoveThatFitAdminBundle:FitModelSpecs:show.html.twig', array(
+                    'fit_model_measurement' => $fit_model_measurement,
+                    'fit_points' => $fit_points,
+                ));
     }
-    
+
+    #------------------------/admin/fit_model_specs/edit/id
+
+    public function editAction($id) {
+        $fit_model_measurement = $this->get('admin.fit_model_measurement')->find($id);
+        $fit_point_values = json_decode($fit_model_measurement->getMeasurementJson(), true);
+        $brands = $this->get('admin.helper.brand')->getBrnadArray();
+        $clothing_types = $this->get('admin.helper.clothing_type')->getArray();
+        $size_specs = $this->get('admin.helper.size')->getDefaultArray();
+        $product_specs = $this->get('admin.helper.product.specification')->getProductSpecification();
+        $fit_points = $this->get('admin.helper.product.specification')->getFitPoints();
+        return $this->render('LoveThatFitAdminBundle:FitModelSpecs:create_new.html.twig', array(
+                    'fit_model_measurement' => $fit_model_measurement,
+                    'fit_point_values' => $fit_point_values,
+                    'fit_points' => $fit_points,
+                    'brands' => $brands,
+                    'clothing_types' => $clothing_types,
+                    'product_specs_json' => json_encode($product_specs),
+                    'size_specs_json' => json_encode($size_specs),
+                ));
+    }
+
 }
