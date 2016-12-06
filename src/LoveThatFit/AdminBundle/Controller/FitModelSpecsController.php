59fa1f0801c5497105e675fb40aa8ab483475af9
waqas muddasir <swm@winCS>
2016-12-06 22:25:40 +0500

    fit model index & show

--- src/LoveThatFit/AdminBundle/Controller/FitModelSpecsController.php
+++ src/LoveThatFit/AdminBundle/Controller/FitModelSpecsController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FitModelSpecsController extends Controller {
 
+    #------------------------/admin/fit_model_specs/create_new
     public function createNewAction(){        
         #return new Response(json_encode($this->get('admin.helper.product.specification')->getFitPoints()));
         $brands = $this->get('admin.helper.brand')->getBrnadArray();
@@ -25,6 +26,7 @@ class FitModelSpecsController extends Controller {
             
          ));        
     }
+    #------------------------/admin/fit_model_specs/save
     public function saveAction(Request $request){        
         $decoded = $request->request->all();
         
@@ -48,5 +50,20 @@ class FitModelSpecsController extends Controller {
         $this->get('admin.fit_model_measurement')->save($fmm);
         return new Response('saved!');
     }
- 
+    #------------------------/admin/fit_model_specs/index
+  public function indexAction(){ 
+        return $this->render('LoveThatFitAdminBundle:FitModelSpecs:index.html.twig', array(
+             'fit_model_measurements'=>$this->get('admin.fit_model_measurement')->findAll(),
+         ));        
+    }
+    #------------------------/admin/fit_model_specs/show/id
+    public function showAction($id){ 
+        $fit_model_measurement=$this->get('admin.fit_model_measurement')->find($id);
+       $fit_points = json_decode($fit_model_measurement->getMeasurementJson(),true);
+       return $this->render('LoveThatFitAdminBundle:FitModelSpecs:show.html.twig', array(
+            'fit_model_measurement' => $fit_model_measurement,
+            'fit_points' => $fit_points,             
+         ));        
+    }
+    
 }
