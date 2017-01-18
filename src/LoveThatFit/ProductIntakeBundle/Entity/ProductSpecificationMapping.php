7144114c7c4df93382aad5f61202e0376a222141
waqas muddasir <swm@winCS>
2017-01-18 23:18:41 +0500

    product intake: csv parsing

--- src/LoveThatFit/ProductIntakeBundle/Entity/ProductSpecificationMapping.php
+++ src/LoveThatFit/ProductIntakeBundle/Entity/ProductSpecificationMapping.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace LoveThatFit\ProductIntakeBundle\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -10,8 +11,8 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Table(name="product_specification_mapping")
  * @ORM\HasLifecycleCallbacks()
  */
-class ProductSpecificationMapping  {
-    
+class ProductSpecificationMapping {
+
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -24,58 +25,61 @@ class ProductSpecificationMapping  {
      */
     protected $title;
 
-     /**
+    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $brand;
-    
-     /**
+
+    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $gender;
-    
-     /**
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    protected $size_title_type;
+
+    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $clothing_type;
-/**
+
+    /**
      * @var string $description
      * @ORM\Column(name="description", type="text", nullable=true)
      */
     private $description;
-    
+
     /**
      * @var string $mapping_json
      * @ORM\Column(name="mapping_json", type="text", nullable=true)
      */
     private $mapping_json;
-    
+
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     protected $mapping_file_name;
-    
+
     /**
      * @ORM\Column(type="datetime")
      */
-    
     protected $created_at;
 
-  
     /**
      * @Assert\File(maxSize="6000000")
      */
     public $file;
 
-    
     /**
      * @var string $disabled
      * @ORM\Column(name="disabled", type="boolean")
      */
     private $disabled;
+
     #-------------------------------------------
-    
-    
+
     /**
      * Get id
      *
@@ -84,6 +88,7 @@ class ProductSpecificationMapping  {
     public function getId() {
         return $this->id;
     }
+
 #-------------------------------------------
     /**
      * Set title
@@ -91,9 +96,9 @@ class ProductSpecificationMapping  {
      * @param string $title
      * @return ProductSpecificationMapping
      */
+
     public function setTitle($title) {
         $this->title = $title;
-
         return $this;
     }
 
@@ -105,13 +110,15 @@ class ProductSpecificationMapping  {
     public function getTitle() {
         return $this->title;
     }
-#-------------------------------------------
+
+#-------------------------------------------    
     /**
      * Set brand
      *
      * @param string $brand
      * @return ProductSpecificationMapping
      */
+
     public function setBrand($brand) {
         $this->brand = $brand;
         return $this;
@@ -124,6 +131,51 @@ class ProductSpecificationMapping  {
      */
     public function getBrand() {
         return $this->brand;
+    }
+
+#-------------------------------------------
+    /**
+     * Set gender
+     *
+     * @param string $gender
+     * @return ProductSpecificationMapping
+     */
+
+    public function setGender($gender) {
+        $this->gender = $gender;
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return string 
+     */
+    public function getGender() {
+        return $this->gender;
+    }
+
+#-------------------------------------------
+    /**
+     * Set size_title_type
+     *
+     * @param string $size_title_type
+     * @return ProductSpecificationMapping
+     */
+
+    public function setSizeTitleType($size_title_type) {
+        $this->size_title_type = $size_title_type;
+
+        return $this;
+    }
+
+    /**
+     * Get size_title_type
+     *
+     * @return string 
+     */
+    public function getSizeTitleType() {
+        return $this->size_title_type;
     }
 
     //----------------------------------------------------------
@@ -155,8 +207,9 @@ class ProductSpecificationMapping  {
      * @param string $mapping_json
      * @return ProductSpecificationMapping
      */
-    public function setMappingJson($mapping_json){
-        $this->mapping_json = $mapping_json;    
+
+    public function setMappingJson($mapping_json) {
+        $this->mapping_json = $mapping_json;
         return $this;
     }
 
@@ -165,20 +218,20 @@ class ProductSpecificationMapping  {
      *
      * @return string 
      */
-    public function getMappingJson(){
+    public function getMappingJson() {
         return $this->mapping_json;
     }
 
-
 #----------------------------------------
-     /**
+    /**
      * Set mapping_file_name
      *
      * @param string $mapping_file_name
      * @return ProductSpecificationMapping
      */
-    public function setMappingFileName($file_name){
-        $this->mapping_file_name = $file_name;    
+
+    public function setMappingFileName($file_name) {
+        $this->mapping_file_name = $file_name;
         return $this;
     }
 
@@ -187,11 +240,12 @@ class ProductSpecificationMapping  {
      *
      * @return string 
      */
-    public function getMappingFileName(){
+    public function getMappingFileName() {
         return $this->mapping_file_name;
     }
-   #-------------------------------------------
-    
+
+    #-------------------------------------------
+
     /**
      * Set created_at
      *
@@ -220,10 +274,10 @@ class ProductSpecificationMapping  {
      * @param boolean $disabled
      * @return ProductSpecificationMapping
      */
-    public function setDisabled($disabled)
-    {
+
+    public function setDisabled($disabled) {
         $this->disabled = $disabled;
-    
+
         return $this;
     }
 
@@ -232,61 +286,54 @@ class ProductSpecificationMapping  {
      *
      * @return boolean 
      */
-    public function getDisabled()
-    {
+    public function getDisabled() {
         return $this->disabled;
     }
 
-   
 //---------------------------------------------------
-    
-  
+
     /**
      * Constructor
      */
-    public function __construct()
-    {
+    public function __construct() {
+        
     }
-      //-------------------------------------------------
+
+    //-------------------------------------------------
     //-------------- Image Upload ---------------------
     //-------------------------------------------------
 
     public function upload() {
-      // the file property can be empty if the field is not required
-      if (null === $this->file) {
-        return;
-      }
+        // the file property can be empty if the field is not required
+        if (null === $this->file) {
+            return;
+        }
         $ext = pathinfo($this->file->getClientOriginalName(), PATHINFO_EXTENSION);
-        $this->mapping_file_name = uniqid() .'.'. $ext;
+        $this->mapping_file_name = uniqid() . '.' . $ext;
         $this->file->move(
                 $this->getUploadRootDir(), $this->mapping_file_name
         );
-      
     }
-  //---------------------------------------------------
 
-    public function getAbsolutePath()
-    {
-      return null === $this->mapping_file_name
-          ? null
-          : $this->getUploadRootDir().'/'.$this->mapping_file_name;
+    //---------------------------------------------------
+
+    public function getAbsolutePath() {
+        return null === $this->mapping_file_name ? null : $this->getUploadRootDir() . '/' . $this->mapping_file_name;
     }
-  //---------------------------------------------------
-    public function getWebPath()
-    {
-      return null === $this->mapping_file_name
-          ? null
-          : $this->getUploadDir().'/'.$this->mapping_file_name;
+
+    //---------------------------------------------------
+    public function getWebPath() {
+        return null === $this->mapping_file_name ? null : $this->getUploadDir() . '/' . $this->mapping_file_name;
     }
-  //---------------------------------------------------
-    protected function getUploadRootDir()
-    {
-      return __DIR__.'/../../../../web/'.$this->getUploadDir();
+
+    //---------------------------------------------------
+    protected function getUploadRootDir() {
+        return __DIR__ . '/../../../../web/' . $this->getUploadDir();
     }
-  //---------------------------------------------------
-    protected function getUploadDir(){    
-      return 'uploads/ltf/products/product_csv';
+
+    //---------------------------------------------------
+    protected function getUploadDir() {
+        return 'uploads/ltf/products/product_csv';
     }
-    
-    
+
 }
