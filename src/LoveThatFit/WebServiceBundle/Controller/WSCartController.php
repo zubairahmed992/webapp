cf9b4fe39070581b8243a97e993fa7adf8fb9e20
haris.khalique <Admin89&!pk>
2017-02-14 20:57:10 +0500

    service changes in ws-show-user-cart

--- src/LoveThatFit/WebServiceBundle/Controller/WSCartController.php
+++ src/LoveThatFit/WebServiceBundle/Controller/WSCartController.php
@@ -96,12 +96,17 @@ class WSCartController extends Controller
         $user = array_key_exists('auth_token', $decoded) ? $this->get('webservice.helper')->findUserByAuthToken($decoded['auth_token']) : null;
         if ($user) {
             $resp = $this->container->get('cart.helper.cart')->getUserCart($user);
-            $base_path = $this->getRequest()->getScheme() . '://' . $this->getRequest()->getHttpHost() . $this->getRequest()->getBasePath() . '/';
-            foreach ($resp as $key => $value) {
-                $resp[$key]['image'] = $base_path . $value['image'];
+            if($resp){
+                $base_path = $this->getRequest()->getScheme() . '://' . $this->getRequest()->getHttpHost() . $this->getRequest()->getBasePath() . '/';
+                foreach ($resp as $key => $value) {
+                    $resp[$key]['image'] = $base_path . $value['image'];
+                }
+
+                $res = $this->get('webservice.helper')->response_array(true, "item found", true, $resp);
+            }else{
+                $res = $this->get('webservice.helper')->response_array(false, 'No Item Found.');
             }
 
-            $res = $this->get('webservice.helper')->response_array(true, json_encode($resp));
         } else {
             $res = $this->get('webservice.helper')->response_array(false, 'User not authenticated.');
         }
