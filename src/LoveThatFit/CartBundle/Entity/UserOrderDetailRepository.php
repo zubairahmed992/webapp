7d4155e8225271c4bea1f49e312cbcf1a29d8b34
Muhammad Umer Javed <umer.javed@centricsource.com>
2017-02-20 22:15:13 +0500

    modify orders export fucntioanity

--- src/LoveThatFit/CartBundle/Entity/UserOrderDetailRepository.php
+++ src/LoveThatFit/CartBundle/Entity/UserOrderDetailRepository.php
@@ -22,4 +22,17 @@ class UserOrderDetailRepository extends EntityRepository
           	->getQuery()
           	->getResult();
 	}
+
+  public function findByOrderIDExport($order_id)
+  {
+    $query = $this->getEntityManager()->createQueryBuilder();
+    return $query->select('ud.qty, ud.item_description, p.control_number, b.name as brand_name')
+            ->from('LoveThatFitCartBundle:UserOrderDetail', 'ud')
+            ->leftJoin("LoveThatFitAdminBundle:ProductItem","i", "WITH","ud.product_item = i.id")
+            ->leftJoin("LoveThatFitAdminBundle:Product","p", "WITH","i.product = p.id")
+            ->leftJoin("LoveThatFitAdminBundle:Brand","b", "WITH","p.brand = b.id")
+            ->where('ud.user_order=:order_id')->setParameter('order_id', $order_id)
+            ->getQuery()
+            ->getResult();
+  }
 }
