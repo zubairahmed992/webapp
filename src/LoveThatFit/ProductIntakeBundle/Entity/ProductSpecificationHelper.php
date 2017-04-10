ec7976d2a27590759b2836cf25b05c8484685216
waqas muddasir <swm@winCS>
2017-04-10 23:17:03 +0500

    PI: fit model max/min actual fixes

--- src/LoveThatFit/ProductIntakeBundle/Entity/ProductSpecificationHelper.php
+++ src/LoveThatFit/ProductIntakeBundle/Entity/ProductSpecificationHelper.php
@@ -344,11 +344,14 @@ class ProductSpecificationHelper {
         foreach ($specs['sizes'][$fit_model_obj->getSize()] as $fit_point => $measure) {
             $fit_model_ratio['fit_model_measurement'][$fit_point] = $specs['sizes'][$fit_model_obj->getSize()][$fit_point];
             $grade_rule = $specs['sizes'][$fit_model_obj->getSize()][$fit_point]['grade_rule_stretch'];
+            
             $fit_model_ratio['fit_model_measurement'][$fit_point]['min_calc'] = $fit_model_fit_points[$fit_point] > 0 ? $fit_model_fit_points[$fit_point] - (2.5 * $grade_rule) : 0;
             $fit_model_ratio['fit_model_measurement'][$fit_point]['ideal_low'] = $fit_model_fit_points[$fit_point] - (0.5 * $grade_rule);
             $fit_model_ratio['fit_model_measurement'][$fit_point]['fit_model'] = $fit_model_fit_points[$fit_point];
             $fit_model_ratio['fit_model_measurement'][$fit_point]['ideal_high'] = $fit_model_fit_points[$fit_point] + (0.5 * $grade_rule);
             $fit_model_ratio['fit_model_measurement'][$fit_point]['max_calc'] = $fit_model_fit_points[$fit_point] > 0 ? $fit_model_fit_points[$fit_point] + (2.5 * $grade_rule) : 0;
+            $fit_model_ratio['fit_model_measurement'][$fit_point]['min_actual'] = $fit_model_ratio['fit_model_measurement'][$fit_point]['min_calc'];
+            $fit_model_ratio['fit_model_measurement'][$fit_point]['max_actual'] = $fit_model_ratio['fit_model_measurement'][$fit_point]['max_calc'];
             #---------------> Calculate ratios
             $fit_model_ratio[$fit_point]['fit_model'] = ($measure['garment_stretch'] > 0 ) ? ($fit_model_fit_points[$fit_point] / $measure['garment_stretch']) : 0;
             $fit_model_ratio[$fit_point]['min_calc'] = ($fit_model_ratio['fit_model_measurement'][$fit_point]['min_calc'] / $fit_model_fit_points[$fit_point]);
