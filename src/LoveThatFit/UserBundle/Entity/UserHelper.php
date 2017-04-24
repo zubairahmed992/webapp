e413d6686090377fdc649abc108d0987c2d02101
sadam-cs <sadam.hussain@centricsource.com>
2017-04-24 17:19:46 +0500

    implemented user logs report

--- src/LoveThatFit/UserBundle/Entity/UserHelper.php
+++ src/LoveThatFit/UserBundle/Entity/UserHelper.php
@@ -1090,4 +1090,11 @@ class UserHelper {
     {
         return $this->repo->findUserList($start_date, $end_date);
     }
+
+    public function findUserById($user_id)
+    {
+        return $this->repo->findOneById($user_id);
+    }
+
+
 }
