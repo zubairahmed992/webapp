7307940a3a3f81f020d5e925cc0076f8032c99b2
Aijaz Ali - SS PHP Team <aijaz.ali@csd.local>
2017-05-04 17:19:38 +0500

    Podio Users Controller one condition is added in it

--- src/LoveThatFit/UserBundle/Controller/PodioUsersController.php
+++ src/LoveThatFit/UserBundle/Controller/PodioUsersController.php
@@ -21,28 +21,32 @@ class PodioUsersController extends Controller {
     	$decoded  = $this->process_request(); 
     	$status = [0,2]; //user podio status 0=pending , 2=failure
     	$podio_users = $this->get('user.helper.podio')->findUserByStatus($status); //get podio pending or failure users    	
+        if($podio_users) {
+            $total_podio_users = count($podio_users);
+        	foreach ($podio_users as $users) {
+                //echo "<pre>"; print_r($users); 
+                $id = $users['id']; //podio user log id
+        		$user_podio = array(
+                    'id' => $users['member_id'],
+                    'email' => ($users['email']) ? $users['email'] : '',
+                    'gender' => ($users['gender']=='f') ? 'Female' : 'Male',
+                    'birth_date' => ($users['birthDate']) ? $users['birthDate']->format('Y-m-d h:i:s') : '',
+                    'created_at' => ($users['member_created']) ? $users['member_created']->format('Y-m-d h:i:s') : '',
+                    'zipcode' => ($users['zipcode']) ? $users['zipcode'] : '',
+                    'base_path' => ($decoded['base_path']) ? $decoded['base_path'] : ''
+                );
+        		//echo "<pre>"; print_r($user_podio); 
 
-    	foreach ($podio_users as $users) {
-            //echo "<pre>"; print_r($users); 
-            $id = $users['id']; //podio user log id
-    		$user_podio = array(
-                'id' => $users['member_id'],
-                'email' => ($users['email']) ? $users['email'] : '',
-                'gender' => ($users['gender']=='f') ? 'Female' : 'Male',
-                'birth_date' => ($users['birthDate']) ? $users['birthDate']->format('Y-m-d h:i:s') : '',
-                'created_at' => ($users['member_created']) ? $users['member_created']->format('Y-m-d h:i:s') : '',
-                'zipcode' => ($users['zipcode']) ? $users['zipcode'] : '',
-                'base_path' => ($decoded['base_path']) ? $decoded['base_path'] : ''
-            );
-    		//echo "<pre>"; print_r($user_podio); 
-
-    		$podio_id = $this->container->get('user.helper.podioapi')->saveUserPodio($user_podio);
-            //echo "podio_id:".$podio_id."<br>";
-    		if($podio_id) {
-                $data = $this->get('user.helper.podio')->updatePodioUsers($id, $podio_id);
-    		}
-    	}
-        die('Podio User Cron Job Service');
+        		$podio_id = $this->container->get('user.helper.podioapi')->saveUserPodio($user_podio);
+                //echo "podio_id:".$podio_id."<br>";
+        		if($podio_id) {
+                    $data = $this->get('user.helper.podio')->updatePodioUsers($id, $podio_id);
+        		}
+        	}
+            die(''.$total_podio_users.' Podio users added by cron job service...');
+        } else {
+            die('No New Podio Users Found...');
+        }
     }
 
     public function asyncCallAction($user_id)
