00f031a663106304dc4e8d3301d6b880828349b1
Shakeel Ahmed. - netStride <shakeel.ahmed@csd.local>
2017-07-21 22:03:19 +0500

    Added module of visitor log: Ticket ID: WEBAP-111

--- src/LoveThatFit/SiteBundle/Entity/VisitorHelper.php
+++ src/LoveThatFit/SiteBundle/Entity/VisitorHelper.php
@@ -66,5 +66,52 @@ class VisitorHelper {
         return $this->repo->findOneByEmail($email);
     }
 
+
+    public function search($data)
+    {
+
+        $draw = isset ( $data['draw'] ) ? intval( $data['draw'] ) : 0;
+        //length
+        $length  = $data['length'];
+        $length  = $length && ($length!=-1) ? $length : 0; 
+        //limit
+        $start   = $data['start']; 
+        $start   = $length ? ($start && ($start!=-1) ? $start : 0) / $length : 0; 
+        //order by
+        $order   = $data['order'];
+        //search data
+        $search  = $data['search'];
+        $filters = [
+            'query' => @$search['value']
+        ];
+
+        $finalData = $this->repo->search($filters, $start, $length, $order);
+        $output = array( 
+            "draw"            => $draw,
+            'recordsFiltered' => count($this->repo->search($filters, 0, false, $order)), 
+            'recordsTotal'    => count($this->repo->search(array(), 0, false, $order)),
+            'data'            => array()
+        );
+
+
+
+        foreach ($finalData as $fData) {
+           
+            $output['data'][] = [ 
+                'id'        => $fData["id"],
+                'email'        => $fData["email"],
+                'ip_address' => $fData["ip_address"],                
+                'created_at'   => ($fData["created_at"]->format('d-m-Y')),
+            ];
+        }
+        return $output;
+    }
+
+     public function findvisitorsList()
+    {
+        return $this->repo->findvisitorsList();
+    }
+
+
 }
 
--- src/LoveThatFit/SiteBundle/Entity/VisitorRepository.php
+++ src/LoveThatFit/SiteBundle/Entity/VisitorRepository.php
@@ -8,6 +8,76 @@ use Doctrine\ORM\EntityRepository;
  * VisitorRepository
  */
 class VisitorRepository extends EntityRepository {
+
+
+
+	 public function search(
+        $data,
+        $page = 0,
+        $max = NULL,
+        $order,
+        $getResult = true
+    ) 
+    {
+        $query = $this->getEntityManager()->createQueryBuilder();
+        $search = isset($data['query']) && $data['query']?$data['query'] : null;
+        $query 
+            ->select('
+                f.id, f.email,
+                f.ip_address, f.created_at'
+            )
+            ->from('LoveThatFitSiteBundle:visitor', 'f');
+            
+        if ($search) {
+            $query 
+                ->andWhere('f.id like :search')
+                ->orWhere('f.email like :search')
+                ->orWhere('f.ip_address like :search')
+                ->orWhere('f.created_at like :search')
+                ->setParameter('search', "%".$search."%");
+        }
+      
+        //$query->groupBy('f.product');
+        if (is_array($order)) {
+            $orderByColumn    = $order[0]['column'];
+            $orderByDirection = $order[0]['dir'];
+            if ($orderByColumn == 0) {
+                $orderByColumn = "f.id";
+            } elseif ($orderByColumn == 1) {
+                $orderByColumn = "f.email";
+            } elseif ($orderByColumn == 2) {
+                $orderByColumn = "f.ip_address";
+            } elseif ($orderByColumn == 3) {
+                $orderByColumn = "f.created_at";
+            }
+
+            $query->OrderBy($orderByColumn, $orderByDirection);
+        }
+        if ($max) {
+            $preparedQuery = $query->getQuery() 
+                ->setMaxResults($max)
+                ->setFirstResult(($page) * $max);
+        } else {
+            $preparedQuery = $query->getQuery(); 
+        }
+
+        return $getResult?$preparedQuery->getResult():$preparedQuery; 
+    }
+
+
+    public function findvisitorsList()
+    {
+        return  $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('
+                f.id, f.email,
+                f.ip_address, f.created_at'
+            )
+            ->from('LoveThatFitSiteBundle:visitor', 'f')          
+            ->OrderBy("f.created_at", "desc")
+            ->getQuery()
+            ->getResult();
+    }
   
 
 }
