43cc78040d20a5d4fa4690849e892f4aff2a243a
sadam-cs <sadam.hussain@centricsource.com>
2017-07-25 15:59:13 +0500

    shop the look and product details tiles functionality along with web service

--- src/LoveThatFit/AdminBundle/Entity/ProductHelper.php
+++ src/LoveThatFit/AdminBundle/Entity/ProductHelper.php
@@ -1817,4 +1817,8 @@ class ProductHelper
 
         return $ids;
     }
+
+    public function getProductDetail($id) {
+        return $this->repo->productDetailForBanner($id);
+    }
 }
--- src/LoveThatFit/WebServiceBundle/Controller/WSCategoryController.php
+++ src/LoveThatFit/WebServiceBundle/Controller/WSCategoryController.php
@@ -29,14 +29,16 @@ class WSCategoryController extends Controller {
     {
         $decoded = $this->get('webservice.helper')->processRequest($this->getRequest());
         $base_path = $this->getRequest()->getScheme() . '://' . $this->getRequest()->getHttpHost() . $this->getRequest()->getBasePath() . '/';
-
+        $user = array_key_exists('auth_token', $decoded) ? $this->get('webservice.helper')->findUserByAuthToken($decoded['auth_token']) : null;
+        $user_id = $user->getId();
+        
         if (array_key_exists('gender', $decoded)) {
             $categoryList = $this->get('admin.helper.Categories')->getCategoryListForService($base_path, $decoded['gender']);
         } else {
             $categoryList = $this->get('admin.helper.Categories')->getCategoryListForService($base_path);
         }
 
-        $bannerList = $this->get('admin.helper.Banner')->getBannerListForService($base_path, 'shop');
+        $bannerList = $this->get('admin.helper.Banner')->getBannerListForService($base_path, 'shop', $user_id);
 
         $bannerconf= array(
             'data' => $bannerList,
