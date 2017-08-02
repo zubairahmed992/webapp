8f726dc4805ce9bbc9d991e945b9db5766d4d2e5
waqas-muddasir-cs <waqas.muddasir@centricsource.com>
2017-08-02 23:27:24 +0500

    PDA1: added

--- src/LoveThatFit/AdminBundle/Controller/AlgorithmController.php
+++ src/LoveThatFit/AdminBundle/Controller/AlgorithmController.php
@@ -10,10 +10,41 @@ use LoveThatFit\AdminBundle\Form\Type\AlgoritumProductTestlType;
 use LoveThatFit\SiteBundle\Comparison;
 use LoveThatFit\SiteBundle\DependencyInjection\FitAlgorithm3;
 use LoveThatFit\SiteBundle\DependencyInjection\FitAlgorithm2;
+use LoveThatFit\SiteBundle\DependencyInjection\PDA1;
 use LoveThatFit\SiteBundle\DependencyInjection\FitAlgorithm2Old;
 
 class AlgorithmController extends Controller {
-    
+
+################################################################
+#   Preference Driven Algorithm 1
+################################################################
+
+    public function pda1IndexAction() {
+        $userForm = $this->createForm(new AlgoritumTestlType());
+        $productForm = $this->createForm(new AlgoritumProductTestlType());
+        return $this->render('LoveThatFitAdminBundle:Algoritm:pda1_index.html.twig', array(
+                    'userForm' => $userForm->createView(),
+                    'productForm' => $productForm->createView(),
+                    'user' => '',
+                ));
+    }
+
+    #------------------------------------------------------------------------------------------
+
+    public function pda1CompareAction($user_id, $product_id, $json = 0) {
+        $product = $this->get('admin.helper.product')->find($product_id);
+        $user = $this->get('user.helper.user')->find($user_id);
+        $fe = new PDA1($user, $product);
+        if ($json == 0) {
+            return $this->render('LoveThatFitAdminBundle:Algoritm:_pda1_comparison.html.twig', array(
+                        'product' => $product, 'user' => $user, 'data' => $fe->getFeedback(),
+                    ));
+        } elseif ($json == 1) {
+            return new Response(json_encode($fe->getFeedback()));
+        } elseif ($json == 2) {
+            return new Response($fe->getStrippedFeedBackJSON());
+        }
+    }    
 ################################################################
 #   Fit Algorithm 3    
 ################################################################
