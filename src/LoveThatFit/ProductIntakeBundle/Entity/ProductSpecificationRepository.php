32f1043ee9213b47829b0f1cfc26dad6bab4e561
asifraza64 <asif.raza@centricsource.com>
2017-08-30 18:27:13 +0500

    PI: Mapping Specs List
    Specs List of Mapping

--- src/LoveThatFit/ProductIntakeBundle/Entity/ProductSpecificationRepository.php
+++ src/LoveThatFit/ProductIntakeBundle/Entity/ProductSpecificationRepository.php
@@ -6,6 +6,16 @@ use Doctrine\ORM\EntityRepository;
 
 
 class ProductSpecificationRepository extends EntityRepository{
-    
+
+    public function findOneByTitle($title) {
+        $query = $this->getEntityManager()
+            ->createQuery("SELECT ps FROM LoveThatFitProductIntakeBundle:ProductSpecification ps WHERE ps.title = :title")
+            ->setParameters(array('title' => $title));
+        try {
+            return $query->getResult();
+        } catch (\Doctrine\ORM\NoResultException $e) {
+            return "null";
+        }
+    }
 
 }
