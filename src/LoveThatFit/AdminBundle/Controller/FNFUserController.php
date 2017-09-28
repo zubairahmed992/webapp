1e1fe85dec43cb6ce0cf1b1fab43c33ebeb84f34
Shakeel Ahmed. - netStride <shakeel.ahmed@csd.local>
2017-09-28 20:22:05 +0500

    Add User in Existing Group

--- src/LoveThatFit/AdminBundle/Controller/FNFUserController.php
+++ src/LoveThatFit/AdminBundle/Controller/FNFUserController.php
@@ -282,23 +282,31 @@ class FNFUserController extends Controller
 
                             $userWithPreviousGroup = $this->get('fnfgroup.helper.fnfgroup')->checkFnfUserToUniqueGroup(implode(",",$userID),$group_type);
 
-                            $ExistingGroup =  $this->get('fnfgroup.helper.fnfgroup')->getExistingFnfGroups($groupTitle);
+                            $ExistingGroup =  $this->get('fnfgroup.helper.fnfgroup')->getExixtGroupDataByName($groupTitle);
+
+                         
+
 
                             
 
                             
 
                             
-                            if(count($userWithPreviousGroup) == 0 && count($ExistingGroup)==0)
+                            if(count($userWithPreviousGroup) == 0)
                             {  
 
 
 
                                 //$userWithPreviousGroup = $this->get('fnfgroup.helper.fnfgroup')->getGroupDataByName($groupTitle);
 
-                            
+                                if(count($ExistingGroup) > 0){
+                               $newGroup = $this->get('fnfgroup.helper.fnfgroup')->UpdateExistingGroup($groupInfoNew,$ExistingGroup[0]['fnfid']);  
+                                   
+
+                                }else{    
 
                                 $newGroup = $this->get('fnfgroup.helper.fnfgroup')->addNewGroup($groupInfoNew);
+                                }
                              
 
                                  $this->get('fnfgroup.helper.fnfgroup')->checkFnfUserUpdate(implode(",",$userID));
@@ -330,7 +338,7 @@ class FNFUserController extends Controller
                         return $this->redirect($this->generateUrl('admin_csv_fnf_create_user'));
                     }
 
-                    if(count($userWithPreviousGroup) == 0 && count($ExistingGroup)==0)
+                    if(count($userWithPreviousGroup) == 0)
                     { 
 
                     $this->get('session')->setFlash('success', 'Group created successfully');
@@ -339,9 +347,7 @@ class FNFUserController extends Controller
 
                 }
 
-            } catch (\Exception $e) {
-                echo $e;
-                exit;
+            } catch (\Exception $e) {                       
                 $this->get('session')->setFlash('warning', 'Invalid File');
                 return $this->redirect($this->generateUrl('admin_csv_fnf_create_user'));
             }
@@ -356,19 +362,19 @@ class FNFUserController extends Controller
 
         }
 
-        $existGroup = ""; 
-       if(count($ExistingGroup) > 0)
+       // $existGroup = ""; 
+       /*if(count($ExistingGroup) > 0)
         {
 
                 $existGroup = 'Group Name ( '.$groupTitle.' ) already exist. Please change the group name to procced.';
 
           
-        }   
+        }   */
 
         return $this->render('LoveThatFitAdminBundle:FNFUser:fnf-upload.html.twig', array(
             'fvfImportform' => $fnfCsvform->createView(),
             'userInPreviousGroup' => trim($existids,", "),
-            'existGroup' => $existGroup,
+            //'existGroup' => $existGroup,
         )
         );
 
