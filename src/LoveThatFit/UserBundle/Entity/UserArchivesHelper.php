1f8732e07371df1d61cb578659e23e9c990bba76
raghibkhesal-cs <raghib@centricsource.com>
2017-10-11 20:15:19 +0500

    Member Photo Retouch - Support Panel Enhancement IOSV3-374

--- src/LoveThatFit/UserBundle/Entity/UserArchivesHelper.php
+++ src/LoveThatFit/UserBundle/Entity/UserArchivesHelper.php
@@ -543,6 +543,13 @@ public function updateRevertedImageStatus($user_id,$hash) {
         if (array_key_exists('version', $data)) {
             $user_archives->setVersion($data['version']);
         }
+        if($data['retouch_image']!=''){
+            $user_archives->setRetouchImage($data['retouch_image']);
+        }else{
+            $user_archives->setRetouchImage(null);
+        }
+
+
         return $this->save($user_archives);
     }
 
