dfaea0579340eac49b3eb339e57994a59902233d
Aijaz Ali - SS PHP Team <aijaz.ali@csd.local>
2017-10-17 23:00:42 +0500

    IOSV3-287: okta login integration in progress

---
+++ src/LoveThatFit/OneloginSamlBundle/Security/Authentication/Provider/SamlProvider.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace LoveThatFit\OneloginSamlBundle\Security\Authentication\Provider;
+
+use LoveThatFit\OneloginSamlBundle\Security\Authentication\Token\SamlToken;
+use LoveThatFit\OneloginSamlBundle\Security\Authentication\Token\SamlTokenFactoryInterface;
+use LoveThatFit\OneloginSamlBundle\Security\Authentication\Token\SamlTokenInterface;
+use LoveThatFit\OneloginSamlBundle\Security\User\SamlUserFactoryInterface;
+use LoveThatFit\OneloginSamlBundle\Security\User\SamlUserInterface;
+use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class SamlProvider implements AuthenticationProviderInterface
+{
+    private $userProvider;
+    private $userFactory;
+    private $tokenFactory;
+    private $entityManager;
+    private $options;
+
+    public function __construct(UserProviderInterface $userProvider, array $options = array())
+    {
+        $this->userProvider = $userProvider;
+        $this->options = array_merge(array(
+            'persist_user' => false
+        ), $options);
+    }
+
+    public function setUserFactory(SamlUserFactoryInterface $userFactory)
+    {
+        $this->userFactory = $userFactory;
+    }
+
+    public function setTokenFactory(SamlTokenFactoryInterface $tokenFactory)
+    {
+        $this->tokenFactory = $tokenFactory;
+    }
+
+    public function setEntityManager($entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function authenticate(TokenInterface $token)
+    {
+        $user = $this->retrieveUser($token);
+
+        if ($user) {
+            $authenticatedToken = $this->tokenFactory->createToken($user, $token->getAttributes(), $user->getRoles());
+            $authenticatedToken->setAuthenticated(true);
+
+            if ($user instanceof SamlUserInterface) {
+                $user->setSamlAttributes($token->getAttributes());
+            }
+
+            return $authenticatedToken;
+        }
+
+        throw new AuthenticationException('The authentication failed.');
+    }
+
+    public function supports(TokenInterface $token)
+    {
+        return $token instanceof SamlTokenInterface;
+    }
+
+    protected function retrieveUser($token)
+    {
+        try {
+            return $this->userProvider->loadUserByUsername($token->getUsername());
+        } catch (UsernameNotFoundException $e) {
+            if ($this->userFactory instanceof SamlUserFactoryInterface) {
+                return $this->generateUser($token);
+            }
+            
+            throw $e;
+        }
+    }
+
+    protected function generateUser($token)
+    {
+        $user = $this->userFactory->createUser($token);
+
+        if ($this->options['persist_user'] && $this->entityManager) {
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
+        return $user;
+    }
+}
---
+++ src/LoveThatFit/OneloginSamlBundle/Security/Authentication/Token/SamlTokenFactoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LoveThatFit\OneloginSamlBundle\Security\Authentication\Token;
+
+interface SamlTokenFactoryInterface
+{
+    /**
+     * Creates a new SAML Token object.
+     *
+     * @param mixed $user
+     * @param array $attributes
+     * @param array $roles
+     *
+     * @return SamlTokenInterface
+     */
+    public function createToken($user, array $attributes, array $roles);
+}
