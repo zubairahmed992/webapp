02386468848b93acf474a8c51a7a3454a39d1965
Aijaz Ali - SS PHP Team <aijaz.ali@csd.local>
2017-10-20 23:29:57 +0500

    IOSV3-409: app store link added in admin panel

---
+++ src/LoveThatFit/AdminBundle/Controller/AppStoreLinkController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace LoveThatFit\AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use LoveThatFit\AdminBundle\Form\Type\DeleteType;
+use LoveThatFit\AdminBundle\Form\Type\AppStoreLinkTypes;
+
+class AppStoreLinkController extends Controller {
+
+    public function addAction() {
+        $app_link = "https://www.selfiestyler.com/";
+        $this->get('admin.helper.appstorelink')->save($app_link);
+        return $this->redirect('/admin/appstorelink/1/edit');
+    }
+
+    public function editAction($id) {
+
+        $entity = $this->get('admin.helper.appstorelink')->find($id);
+
+        if(!$entity){       
+            $this->get('session')->setFlash('warning', 'The app store link can not be Created!');
+        }else{
+            $form = $this->createForm(new AppStoreLinkTypes('edit',$entity), $entity);
+            $deleteForm = $this->createForm(new DeleteType(), $entity);
+        }
+        return $this->render('LoveThatFitAdminBundle:AppStoreLink:edit.html.twig', array(
+                    'form' => $form->createView(),
+                    'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity
+                    ));
+    }
+
+    public function updateAction(Request $request, $id) {
+
+        $entity = $this->get('admin.helper.appstorelink')->find($id);
+        
+        $form = $this->createForm(new AppStoreLinkTypes('edit',$entity), $entity);
+
+        $form->bind($request);
+
+        $app_store_link = $request->request->get('app_store_link');
+        
+        $app_link = $app_store_link['app_link'];
+
+        $this->get('admin.helper.appstorelink')->update($id, $app_link);
+        
+        $deleteForm = $this->createForm(new DeleteType(), $entity);        
+
+        return $this->render('LoveThatFitAdminBundle:AppStoreLink:edit.html.twig', array(
+                    'form' => $form->createView(),
+                    'delete_form' => $deleteForm->createView(),
+                    'entity' => $entity
+            ));
+    }
+}
