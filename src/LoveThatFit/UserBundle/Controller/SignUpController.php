c7f9af91c15a99fcb333b8a693517815aba586f3
Shakeel Ahmed. - netStride <shakeel.ahmed@csd.local>
2017-10-23 16:33:42 +0500

    All updated of Ticket id 437

--- src/LoveThatFit/UserBundle/Controller/SignUpController.php
+++ src/LoveThatFit/UserBundle/Controller/SignUpController.php
@@ -12,7 +12,7 @@ class SignUpController extends Controller {
 
 #>>>>>>>>>>>>>>>>>>>>>> Replace with new registration >>>>>>>>>>>>>>>>>>
     public function registrationAction() {           
-
+ 
         return $this->render('LoveThatFitUserBundle:SignUp:signup.html.twig');
     }
 
